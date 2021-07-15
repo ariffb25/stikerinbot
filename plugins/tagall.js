e7828a66af8fa1076c8fcb2e77139033dafe8318
ariffb <arif.febrianto.755@gmail.com>
2021-07-14 22:43:47 -0700

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,9 +1,12 @@
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
+  m.reply(text + '\n┌─〔 Tag All 〕\n' + users.map(v => '├ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
     contextInfo: { mentionedJid: users }
   })
 }
+}
+handler.help = ['tagall']
+handler.tags = ['group']
 handler.command = ['tagall']
 
 handler.admin = true
