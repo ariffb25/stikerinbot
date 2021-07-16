1e5c3561c43a47fa19b83d68bc15d1436e128bbe
OneTzy <54070834+RidwanXyZ@users.noreply.github.com>
2021-07-16 19:59:35 +0700

    update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 function handler(m) {
-  this.sendContact(m.chat, '6283128734012', 'ariffb', m)
+  this.sendContact(m.chat, '6289514791129', '𝑅𝑖𝑑𝑤𝑎𝑛𝑋𝑦𝑍', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
