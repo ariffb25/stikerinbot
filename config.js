d046679646fd9fd94130bdb68cb3ec829a8d32db
OneTzy <54070834+RidwanXyZ@users.noreply.github.com>
2021-07-16 19:45:33 +0700

    Okeg

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['6283128734012'] // Letakan nomor kamu disini
+global.owner = ['6289514791129'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
@@ -17,17 +17,17 @@ global.APIKeys = { // APIKey nya disini
   // 'https://website': 'apikey'
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
-  'https://pencarikode.xyz': 'APIKEY',
-  'https://api.xteam.xyz': 'apikey kamu',
+  'https://pencarikode.xyz': 'pais',
+  'https://api.xteam.xyz': 'MIMINETBOT',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
 }
 
 // Sticker WM
-global.packname = '❤️'
-global.author = 'ariffb'
+global.packname = 'jemBOT'
+global.author = 'Ridwan              '
 
-global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
+global.multiplier = 299 // Semakin tinggi, semakin sulit naik level
 
 let chalk = require('chalk')
 let file = require.resolve(__filename)
