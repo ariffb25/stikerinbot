a3fa6a6c1811e398753b61de262d286b0c5837e3
OneTzy <54070834+RidwanXyZ@users.noreply.github.com>
2021-07-16 19:50:20 +0700

    update _antitroli.js

--- plugins/_antitroli.js
+++ plugins/_antitroli.js
@@ -2,10 +2,10 @@ let handler = m => m
 
 handler.all = async function (m) {
     if (m.message && m.isBaileys && m.quoted && m.quoted.mtype === 'orderMessage' && !(m.quoted.token && m.quoted.orderId)) {
-        m.reply('Troli Terdeteksi\n\n' + require('util').format(m.key))
+       /* m.reply('Troli Terdeteksi\n\n' + require('util').format(m.key))
         await this.modifyChat(m.chat, 'clear', {
             includeStarred: false
-        }).catch(console.log)
+        }).catch(console.log)*/
         this.reply(global.owner[0] + '@s.whatsapp.net', `
 Pelaku pengirim troli @${m.sender.split`@`[0]}
 ID: ${m.isGroup ? m.chat : m.sender}
