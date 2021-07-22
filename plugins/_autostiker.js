2311cdb008a7831240cef9242a57a89e7e939124
Ariffb <arif.febrianto.755@gmail.com>
2021-07-22 20:00:48 +0700

    Update _autostiker.js

--- plugins/_autostiker.js
+++ plugins/_autostiker.js
@@ -1,26 +1,36 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
+const uploadFile = require('../lib/uploadFile')
+const uploadImage = require('../lib/uploadImage')
+let { webp2png } = require('../lib/webp2mp4')
 let handler = m => m
 
 handler.before = async function (m) {
     let chat = global.db.data.chats[m.chat]
-    if (chat.stiker && !chat.isBanned) {
+    if (chat.stiker && !chat.isBanned && !m.fromMe && !m.isBaileys) {
         // try {
         let q = m
+        let stiker = false
         let mime = (q.msg || q).mimetype || ''
         if (/webp/.test(mime)) return
         if (/image/.test(mime)) {
-            let img = await q.download().catch(_ => _)
-            if (!img) throw false
-            stiker = await sticker(img, false, global.packname, global.author).catch(_ => _)
-            await this.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m }).catch(_ => _)
-        } if (/video/.test(mime)) {
+            let img = await q.download()
+            let link = await uploadImage(img)
+            if (!img) return
+            stiker = await sticker(0, link, global.packname, global.author)
+        } else if (/video/.test(mime)) {
             if ((q.msg || q).seconds > 11) throw 'Maksimal 10 detik!'
             let img = await q.download()
-            if (!img) throw false
-            stiker = await sticker(img, false, global.packname, global.author).catch(_ => _)
-            await this.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m }).catch(_ => _)
+            let link = await uploadFile(img)
+            if (!img) return
+            stiker = await sticker(0, link, global.packname, global.author)
+        } else if (m.text) {
+            if (isUrl(m.text)) stiker = await sticker(false, m.text, global.packname, global.author)
+            else return
         }
+        if (stiker) await this.sendMessage(m.chat, stiker, 'stickerMessage', {
+            quoted: m
+        })
         // } finally {
         //     if (stiker) {
         //     }
@@ -29,3 +39,7 @@ handler.before = async function (m) {
     return true
 }
 module.exports = handler
+
+const isUrl = (text) => {
+    return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
+}
