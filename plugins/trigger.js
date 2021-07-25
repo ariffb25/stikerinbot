6e0046277fe232771fac5975a6b4206ba4748528
ariffb <arif.febrianto.755@gmail.com>
2021-07-25 20:56:09 +0700

    menggabungkan ttpdark ke ttp

--- plugins/trigger.js
+++ plugins/trigger.js
@@ -1,5 +1,3 @@
-//kok cewe gw jadi cuek yahh? daritadi pagi gw chat ga dibales cuman di read doang
-
 const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
@@ -8,7 +6,7 @@ let handler = async (m, { conn }) => {
     avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
   })
   let stiker = await sticker(null, marah, global.packname, global.author)
- if (stiker) return conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+  if (stiker) return conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
   throw stiker.toString()
--- plugins/ttp.js
+++ plugins/ttp.js
@@ -27,10 +27,20 @@ let handler = async (m, { conn, text, command }) => {
     })
     throw stiker.toString()
   }
+  if (/4$/i.test(command)) {
+    let url = await fetch(global.API('https://salism3api.pythonanywhere.com', '/text2img/', { text: teks, outlineColor: '255,0,0,255', textColor: '0,0,0,255' }))
+    res = await url.json()
+    stick = res.image
+    let stiker = await sticker(null, stick, global.packname, global.author)
+    if (stiker) return await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+      quoted: m
+    })
+    throw stiker.toString()
+  }
 }
-handler.help = new Array(3).fill('ttp').map((v, i) => v + (i + 1) + ' <teks>')
+handler.help = new Array(4).fill('ttp').map((v, i) => v + (i + 1) + ' <teks>')
 handler.tags = ['sticker']
 
-handler.command = /^ttp[1-3]?$/i
+handler.command = /^ttp[1-4]?$/i
 
 module.exports = handler
