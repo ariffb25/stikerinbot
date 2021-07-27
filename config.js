ed0b101bf4e72a1c9ccc0576dba0471836643f75
Hidayatilman <87686497+Hidayatilman@users.noreply.github.com>
2021-07-28 07:07:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['6283128734012'] // Letakan nomor kamu disini
+global.owner = ['6281351047727'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
@@ -26,7 +26,7 @@ global.APIKeys = { // APIKey nya disini
 
 // Sticker WM
 global.packname = '❤️'
-global.author = 'ariffb'
+global.author = 'Ilman'
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
 
