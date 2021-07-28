9623aa1697d8b323de181d8a2338b3b4b9b759cc
Ftwrr <76021496+Ftwrr@users.noreply.github.com>
2021-07-27 21:01:40 -0700

    leveling pake canvacord

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -2,27 +2,38 @@ let handler = m => m
 
 let levelling = require('../lib/levelling')
 handler.before = m => {
-        let user = global.db.data.users[m.sender]
-        if (!user.autolevelup) return !0
-        let before = user.level * 1
-        while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-        let role = ((user.level >= 1) && (user.level <= 10)) ? 'Bronze'
-                : ((user.level >= 10) && (user.level <= 20)) ? 'Bronze I'
-                        : ((user.level >= 20) && (user.level <= 30)) ? 'Silver'
-                                : ((user.level >= 30) && (user.level <= 40)) ? 'Silver I'
-                                        : ((user.level >= 40) && (user.level <= 50)) ? 'Gold'
-                                                : ((user.level >= 50) && (user.level <= 60)) ? 'Platinum'
-                                                        : ((user.level >= 60) && (user.level <= 70)) ? 'Diamond'
-                                                                : ((user.level >= 70) && (user.level <= 80)) ? 'Master'
-                                                                        : 'Pinnacle'
+  let user = global.db.data.users[m.sender]
+  let pp = './src/avatar_contact.png'
+  let who = m.sender
+  let discriminator = who.substring(9, 13)
+  try {
+    pp = await conn.getProfilePicture(who)
+  } catch (e) {
 
-        if (before !== user.level) {
-                m.reply(`
-Selamat, kamu telah naik level!
-*${before}* -> *${user.level}*
-gunakan *.profile* untuk mengecek
-	`.trim())
-                user.role = role
+  } finally {
+  if (!user.autolevelup) return !0
+  let before = user.level * 1
+  while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+        
+  if (before !== user.level) {
+    let rank = await new canvacord.Rank()
+      .setAvatar(pp)
+      .setLevel(user.level)
+      .setCurrentXP(user.exp - min)
+      .setRequiredXP(xp)
+      .setProgressBar("#FFFFFF", "COLOR")
+      .setUsername(conn.getName(who))
+      .setDiscriminator(discriminator);
+      rank.build()
+      .then(data => {
+    conn.sendFile(m.chat, data, 'rank.png', `_*Level Up!*_\n_${before}_ -> _${user.level}_`.trim(), m, false, { thumbnail: Buffer.alloc(0) })
+    });
+//            m.reply(`
+//_*Level Up!*_
+//_${before}_ -> _${user.level}_
+//  `.trim())
+            user.role = role
+          }
         }
 }
 
--- plugins/levelup.js
+++ plugins/levelup.js
@@ -1,40 +1,60 @@
 let levelling = require('../lib/levelling')
+const canvacord = require("canvacord");
+let handler = async (m, { conn, usedPrefix }) => {
+  let pp = './src/avatar_contact.png'
+  let who = m.sender
+  let discriminator = who.substring(9, 13)
+  try {
+    pp = await conn.getProfilePicture(who)
+  } catch (e) {
 
-let handler = m => {
+  } finally {
   let user = global.db.data.users[m.sender]
   if (!levelling.canLevelUp(user.level, user.exp, global.multiplier)) {
     let { min, xp, max } = levelling.xpRange(user.level, global.multiplier)
-    throw `
-Level *${user.level} (${user.exp - min}/${xp})*
-Kurang *${max - user.exp}* lagi!
-`.trim()
+    let rank = await new canvacord.Rank()
+      .setAvatar(pp)
+      .setLevel(user.level)
+      .setCurrentXP(user.exp - min)
+      .setRequiredXP(xp)
+      .setProgressBar("#FFFFFF", "COLOR")
+      .setUsername(conn.getName(who))
+      .setDiscriminator(discriminator);
+      rank.build()
+      .then(data => {
+    conn.sendFile(m.chat, data, 'rank.png', `Level *${user.level} (${user.exp - min}/${xp})*\nKurang *${max - user.exp}* lagi!`.trim(), m, false, { thumbnail: Buffer.alloc(0) })
+    });
+//    throw `
+//Level *${user.level} (${user.exp - min}/${xp})*
+//Kurang *${max - user.exp}* lagi!
+//`.trim()
   }
   let before = user.level * 1
   while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-  let role = ((user.level >= 1) && (user.level <= 10)) ? 'Bronze'
-    : ((user.level >= 10) && (user.level <= 20)) ? 'Bronze I'
-      : ((user.level >= 20) && (user.level <= 30)) ? 'Silver'
-        : ((user.level >= 30) && (user.level <= 40)) ? 'Silver I'
-          : ((user.level >= 40) && (user.level <= 50)) ? 'Gold'
-            : ((user.level >= 50) && (user.level <= 60)) ? 'Platinum'
-              : ((user.level >= 60) && (user.level <= 70)) ? 'Diamond'
-                : ((user.level >= 70) && (user.level <= 80)) ? 'Master'
-                  : 'Pinnacle'
-
   if (before !== user.level) {
-    m.reply(`
-Selamat, kamu telah naik level!
-*${before}* -> *${user.level}*
-gunakan *.profile* untuk mengecek
-gunakan *.on levelup* untuk naik level otomatis
-	`.trim())
-    user.role = role
-  }
+    let rank = await new canvacord.Rank()
+      .setAvatar(pp)
+      .setLevel(user.level)
+      .setCurrentXP(user.exp - min)
+      .setRequiredXP(xp)
+      .setProgressBar("#FFFFFF", "COLOR")
+      .setUsername(conn.getName(who))
+      .setDiscriminator(discriminator);
+      rank.build()
+      .then(data => {
+    conn.sendFile(m.chat, data, 'rank.png', `_*Level Up!*_\n_${before}_ -> _${user.level}_`.trim(), m, false, { thumbnail: Buffer.alloc(0) })
+    });
+//            m.reply(`
+//_*Level Up!*_
+//_${before}_ -> _${user.level}_
+//  `.trim())
+        }
+      }
 }
 
 handler.help = ['levelup']
 handler.tags = ['xp']
 
-handler.command = /^levelup$/i
+handler.command = /^level(|up)$/i
 
 module.exports = handler
