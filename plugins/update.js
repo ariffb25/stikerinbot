c4c260386903a18c10be609c6eb6a98cdcb4dd47
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-07-28 20:50:55 +0530

    update command 'Tag' Fixed ✅️

--- plugins/update.js
+++ plugins/update.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, text, isROwner }) => {
   }
 }
 handler.help = ['update']
-handler.tags = ['host']
+handler.tags = ['owner']
 handler.command = /^update$/i
 handler.owner = true
 handler.mods = false
