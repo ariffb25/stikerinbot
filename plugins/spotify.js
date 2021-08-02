c36460eec8bbf1b248e476925d08c41c02d1f1ef
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-08-02 19:34:34 +0530

    updated credits ✅️

--- plugins/spotify.js
+++ plugins/spotify.js
@@ -1,4 +1,4 @@
-// Made By github.com/TOXIC-DEVIL
+// Made By github.com/TOXIC-DEVIL && github.com/ftwrr
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
   if (!text) throw `Masukkan judul musik!`
@@ -6,7 +6,7 @@ let handler = async(m, { conn, text }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if(!json.data[0]) throw json
-  let { title, artists, album, thumb, url, preview_mp3 } = json.data[0]
+  let { title, artists, album, thumb, url, preview_mp3 } = json.data[0] 
 let spotifyinfo = `✨️ *Title:* ${title}
 🗣️ *Artists:* ${artists}
 🎆️ *Album:* ${album}
@@ -14,10 +14,10 @@ let spotifyinfo = `✨️ *Title:* ${title}
 💚️ *Direct URL:* ${preview_mp3}\n\n@TOXIC-DEVIL`
 
   await conn.sendFile(m.chat, thumb, '', spotifyinfo, m)
-  await conn.sendFile(m.chat, preview_mp3, 'spotify.mp3', spotifyinfo, m)
+  await conn.sendFile(m.chat, preview_mp3, 'spotify.mp3', spotifyinfo, m) // github.com/ftwrr
 }
 handler.help = ['spotify <query>']
 handler.tags = ['internet']
 handler.command = /^(spotify|music)$/i
-// Made By github.com/TOXIC-DEVIL
+// Made By github.com/TOXIC-DEVIL && github.com/ftwrr
 module.exports = handler
