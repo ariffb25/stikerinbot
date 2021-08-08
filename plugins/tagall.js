65c88296b759fb0948f2efd24839b4c563915131
ariffb <arif.febrianto.755@gmail.com>
2021-08-08 19:16:04 +0700

    tagall

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
-  m.reply(text ? `${text}\n` : '' + '\n┌─〔 Tag All 〕\n' + users.map(v => '├ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
+  m.reply(`${text ? `${text}\n` : ''}┌─〔 Tag All 〕\n` + users.map(v => '├ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
     contextInfo: { mentionedJid: users }
   })
 }
