b8cd26f20cb761f92752625aa16761d074d36667
ariffb <arif.febrianto.755@gmail.com>
2021-08-15 09:53:00 +0700

    button respon

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -12,15 +12,15 @@ let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) =
     let _thumb = {}
     try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
     catch (e) { }
-    if (!isLimit) conn.sendVideo(m.chat, dl_link, `
-  *Judul:* ${title}
-  *Ukuran File:* ${filesizeF}
+    if (!isLimit) await conn.sendVideo(m.chat, dl_link, `
+*Judul:* ${title}
+*Ukuran File:* ${filesizeF}
   `.trim(), m, {
       ..._thumb,
       asDocument: chat.useDocument
     })
   } catch (e) {
-    await conn.sendButton(m.chat, 'gagal mendapatkan url, coba lagi?', '', 'YA', `${usedPrefix + command} ${args[0]}`)
+    return await conn.sendButton(m.chat, 'gagal mendapatkan url, coba lagi?', '', 'YA', `${usedPrefix + command} ${args[0]}`)
   }
 }
 handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
