02c39b295a1c181dfc2fe1ae81901c19fe6ca563
ariffb <arif.febrianto.755@gmail.com>
2021-09-07 15:58:07 +0700

    Update jadian.js

--- plugins/jadian.js
+++ plugins/jadian.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, participants, command, usedPrefix, command }) => {
+let handler = async (m, { conn, participants, command, usedPrefix }) => {
     let member = participants.map(u => u.jid)
     let orang
     if (/ku/i.test(command)) orang = m.sender
@@ -6,7 +6,6 @@ let handler = async (m, { conn, participants, command, usedPrefix, command }) =>
     let jodoh = member[Math.floor(Math.random() * member.length)]
     let jawab = `@${orang.replace(/@.+/, '')} ❤️ @${jodoh.replace(/@.+/, '')}`.trim()
     let mentionedJid = [orang, jodoh]
-    conn.reply(m.chat, jawab, m, { contextInfo: { mentionedJid } })
     await conn.sendButton(m.chat, jawab, '© stikerin', `${command}`, usedPrefix + command, m, { contextInfo: { mentionedJid } })
 }
 handler.help = ['jodohin', 'jodohku']
