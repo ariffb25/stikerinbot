32f38b3282946df733da28ae57faed7d460078df
ariffb <arif.febrianto.755@gmail.com>
2021-09-08 08:00:01 +0700

    tebak kabupaten

---
+++ plugins/tebakkabupaten.js
@@ -0,0 +1,33 @@
+let fetch = require('node-fetch')
+let timeout = 120000
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tebakkabupaten = conn.tebakkabupaten ? conn.tebakkabupaten : {}
+    let id = m.chat
+    if (id in conn.tebakkabupaten) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakkabupaten[id][0])
+        throw false
+    }
+    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakkabupaten.json')
+    if (!res.ok) throw eror
+    let data = await res.json()
+    let json = data[Math.floor(Math.random() * data.length)]
+    let caption = `
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}tekb untuk bantuan
+Bonus: ${poin} XP
+`.trim()
+    conn.tebakkabupaten[id] = [
+        await conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), caption, '© stikerin', 'Bantuan', '.teka', m),
+        json, poin,
+        setTimeout(async () => {
+            if (conn.tebakkabupaten[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.title}*`, '© stikerin', 'Tebak Kabupaten', '.tebakkabupaten', conn.tebakkabupaten[id][0])
+            delete conn.tebakkabupaten[id]
+        }, timeout)
+    ]
+}
+handler.help = ['tebakkabupaten']
+handler.tags = ['game']
+handler.command = /^tebakkabupaten/i
+
+module.exports = handler
---
+++ plugins/tebakkabupaten_hint.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn }) => {
+    conn.tebakkabupaten = conn.tebakkabupaten ? conn.tebakkabupaten : {}
+    let id = m.chat
+    if (!(id in conn.tebakkabupaten)) throw false
+    let json = conn.tebakkabupaten[id][1]
+    let ans = json.title.trim()
+    let clue = ans.replace(/[AIUEOaiueo]/g, '_')
+    conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.tebakkabupaten[id][0])
+}
+handler.command = /^teka$/i
+handler.limit = true
+module.exports = handler
