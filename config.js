8d41d7e3c756a6763be99c7cb2cae6327ae724ab
ROMIMUHAROM <78423844+ROMIMUHAROM@users.noreply.github.com>
2021-09-15 22:09:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['6283128734012', '6288709645535'] // Letakan nomor kamu disini
+global.owner = ['6281291282733', '6281291282733'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
@@ -29,7 +29,7 @@ global.APIKeys = { // APIKey nya disini
 
 // Sticker WM
 global.packname = '❤️'
-global.author = 'ariffb'
+global.author = 'Rominaru'
 
 global.wait = '_*tunggu..*_'
 global.eror = '_*Server Error*_'
