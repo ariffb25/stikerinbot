fcef0515aa5feacec944c7ba046701ca950a946f
ROMIMUHAROM <78423844+ROMIMUHAROM@users.noreply.github.com>
2021-09-15 22:16:57 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
-┌〔 Donasi • Emoney 〕
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+┌〔 Donasi • Pulsa 〕
+├ 089638023602
+├ 081291282733
 └────
 `.trim(), '© stikerin', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
