44cec9e2857213795fd0c6c3f9ed1722aa51df3a
ROMIMUHAROM <78423844+ROMIMUHAROM@users.noreply.github.com>
2021-09-15 22:20:30 +0700

    Add files via upload

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,43 +1,7 @@
-const { MessageType } = require('@adiwajshing/baileys')
-const PhoneNumber = require('awesome-phonenumber')
-async function handler(m) {
-  let name = 'ariffb'
-  number = owner[0].replace(/[^0-9]/g, '')
-  let njid = number + '@s.whatsapp.net'
-  let onW = await this.isOnWhatsApp(njid) || { isBusiness: false }
-
-  let name2 = 'andrian'
-  number2 = owner[1].replace(/[^0-9]/g, '')
-  let njid2 = number2 + '@s.whatsapp.net'
-  let onW2 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
-
-  this.sendMessage(m.chat, {
-    contacts: [{
-      displayname: name, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name.replace(/\n/g, '\\n')};;;
-FN:${name.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}${onW.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid].vname || this.getName(njid)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }, {
-      displayname: name2, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name2.replace(/\n/g, '\\n')};;;
-FN:${name2.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number2}:${PhoneNumber('+' + number2).getNumber('international')}${onW2.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid2].vname || this.getName(njid2)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid2)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }]
-  }, MessageType.contactsArray, { quoted: m })
+function handler(m) {
+  // Ini cuma contoh, jgn di uncomment -_-
+  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
+  this.sendContact(m.chat, '6281291282733', 'Rominaru', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
