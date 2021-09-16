7fa46d2cea7b9cc417eb11b9faf2bcf38e69fc35
ROMIMUHAROM <78423844+ROMIMUHAROM@users.noreply.github.com>
2021-09-16 21:54:21 +0700

    Update and rename Ilman.js to Oni.js

--- plugins/Oni.js
+++ plugins/Oni.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-Elaina-chan Punya Ilman Onii-chan  >///<
+Xiao wu Punya Romi Onii-chan  >///<
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /Elaina/i
+handler.customPrefix = /xiao wu/i
 handler.command = new RegExp
 
 module.exports = handler
