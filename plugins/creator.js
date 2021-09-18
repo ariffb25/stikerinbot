06ed56c323003b0872211e603c2caf9aacfd1224
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-18 07:57:05 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,43 +1,24 @@
-const { MessageType } = require('@adiwajshing/baileys')
-const PhoneNumber = require('awesome-phonenumber')
-async function handler(m) {
-  let name = 'ariffb'
-  number = owner[0].replace(/[^0-9]/g, '')
-  let njid = number + '@s.whatsapp.net'
-  let onW = await this.isOnWhatsApp(njid) || { isBusiness: false }
-
-  let name2 = 'andrian'
-  number2 = owner[1].replace(/[^0-9]/g, '')
-  let njid2 = number2 + '@s.whatsapp.net'
-  let onW2 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
-
-  this.sendMessage(m.chat, {
-    contacts: [{
-      displayname: name, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name.replace(/\n/g, '\\n')};;;
-FN:${name.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}${onW.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid].vname || this.getName(njid)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }, {
-      displayname: name2, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name2.replace(/\n/g, '\\n')};;;
-FN:${name2.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number2}:${PhoneNumber('+' + number2).getNumber('international')}${onW2.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid2].vname || this.getName(njid2)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid2)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }]
-  }, MessageType.contactsArray, { quoted: m })
+/*
+* Made by Rizxyu
+* Hehehehehhehehe
+*/
+let { MessageType }= require('@adiwajshing/baileys')
+let { contactsArray } = MessageType
+let handler = function (m, { conn }) {
+   conarray = []
+ownerContact = ['0']//Put your number here
+  for (let i of ownerContact.map(v => v + '@s.whatsapp.net')) {
+ vname = conn.contacts[i] != undefined ? conn.contacts[i].vname || conn.contacts[i].notify : undefined
+  conarray.push({
+"displayName": 'peler',//put your name
+"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:${vname ? `${vname}` : `${conn.user.name}`}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+})
+}
+conn.sendMessage(m.chat, {
+"displayName": `${conarray.length} kontak`,
+"contacts": conarray 
+}, 'contactsArrayMessage', { quoted: m })
+conn.sendMessage(m.chat, 'ini contact owner dan team',MessageType.text, { quoted: m} )
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
