d3b9622f85aa05eae1293662440f57d979fdfb60
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-21 15:25:47 +0700

    Delete say.js

--- plugins/say.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `uhm. teksnya mana?\n\ncontoh:\n${usedPrefix + command} halo`
-    conn.reply(m.chat, text, null)
-}
-handler.help = ['say <teks>']
-handler.tags = ['tools']
-handler.command = /^(say)$/i
-
-module.exports = handler
