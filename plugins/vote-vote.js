e1fd375420a77eedda9064118906a8b7db555ccd
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-21 16:36:15 +0700

    Update vote-vote.js

--- plugins/vote-vote.js
+++ plugins/vote-vote.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     let id = m.chat
     conn.vote = conn.vote ? conn.vote : {}
     if (!(id in conn.vote)) {
-        await conn.sendButton(m.chat, `_*tidak ada voting digrup ini!*_`, '© stikerin', 'MULAI VOTE', `${usedPrefix}mulaivote`, m)
+        await conn.sendButton(m.chat, `_*tidak ada voting digrup ini!*_`, 'Silahkan klik di bawah!', 'Mulai Vote', `${usedPrefix}mulaivote`, m)
         throw false
     }
     let isVote = conn.vote[id][1].concat(conn.vote[id][2])
@@ -30,7 +30,7 @@ ${devote.map(u => '@' + u.split('@')[0]).join('\n')}
 
 _by ariffb_
     `.trim()
-    await conn.send2Button(m.chat, caption, '© stikerin', 'UPVOTE', `${usedPrefix}upvote`, 'DEVOTE', `${usedPrefix}devote`, m, { contextInfo: { mentionedJid } })
+    await conn.send2Button(m.chat, caption, 'Silahkan klik di bawah!', 'Upvote', `${usedPrefix}upvote`, 'Devote', `${usedPrefix}devote`, m, { contextInfo: { mentionedJid } })
 }
 handler.help = ['upvote', 'devote']
 handler.tags = ['vote']
