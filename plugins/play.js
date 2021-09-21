c6a40df3571ae99aad39c2edebc2dc3da85d2c2f
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-21 20:16:24 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -25,7 +25,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (yt === false) throw 'semua server gagal'
   if (yt2 === false) throw 'semua server gagal'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
-  await conn.send2ButtonLoc(m.chat, await (await fetch(vid.url)).buffer(), `
+  await conn.send2ButtonLoc(m.chat, await (await fetch(thumbnail)).buffer(), `
     *🌹YouTube🌹*
     
 🔖 *Judul:* ${title}
