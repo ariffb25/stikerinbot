040da7952e74781bb2dbb89f4ec697c82f672ff6
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-23 23:02:04 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
   if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} hai`
-  let res = await fetch(global.API('pencarikode', '/api/simsimii', { text: encodeURIComponent(text) }, 'apikey'))
+  let res = await fetch('')
   if (!res.ok) throw eror
   let json = await res.json()
   if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin jadi gatau t_t custom pesannya di https://simsimi.com/teach')
-  await m.reply(`*Simi:* ${json.result}`)
+  await m.reply(`*Simi:* ${json.result.answer}`)
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
 handler.tags = ['fun']
