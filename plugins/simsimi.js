7718b1717c7f96645df4b90d21aaf82ec3e05c95
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-23 23:03:14 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
   if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} hai`
-  let res = await fetch('')
+  let res = await fetch('https://ariarestapii.herokuapp.com/api/simsimi?query={text}&apikey=aria')
   if (!res.ok) throw eror
   let json = await res.json()
   if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin jadi gatau t_t custom pesannya di https://simsimi.com/teach')
