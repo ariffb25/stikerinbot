d1840d95986c5ae65a4fc4157a5db0a977cf266e
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-23 19:56:17 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,27 +1,5 @@
-const { MessageType } = require('@adiwajshing/baileys')
-const PhoneNumber = require('awesome-phonenumber')
-async function handler(m) {
-  let name = 'Aria Putra Pratama'
-  number = owner[0].replace(/[^0-9]/g, '')
-  let njid = number + '@s.whatsapp.net'
-  let onW = await this.isOnWhatsApp(njid) || { isBusiness: false }
-  
-  this.sendMessage(m.chat, {
-    contacts: [{
-      displayname: name, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name.replace(/\n/g, '\\n')};;;
-FN:${name.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}${onW.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid].vname || this.getName(njid)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }
-    }]
-  }, MessageType.contactsArray, { quoted: m })
+function handler(m) {
+  this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
