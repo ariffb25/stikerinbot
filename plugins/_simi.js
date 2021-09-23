81448b4d403453c75640cd049d87820b4b3ee080
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-23 23:08:06 +0700

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -6,11 +6,11 @@ handler.before = async (m) => {
     if (chat.simi && !chat.isBanned && !m.isGroup) {
         if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        let res = await fetch(global.API('pencarikode', '/api/simsimii', { text: encodeURIComponent(m.text) }, 'apikey'))
+        let res = await fetch('https://ariarestapii.herokuapp.com/api/simsimi?query=${text}&apikey=aria')
         if (!res.ok) return m.reply(eror)
         let json = await res.json()
         if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin, ajarin di https://simsimi.com/teach')
-        else await m.reply(`*Simi:* ${json.result}`)
+        else await m.reply(`*Simi:* ${json.result.answer}`)
         return !0
     }
     return true
