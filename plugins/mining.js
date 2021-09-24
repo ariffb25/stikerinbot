37e956001f01d7b589caf5ab666872070868ec18
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-24 22:42:20 +0700

    Update mining.js

--- plugins/mining.js
+++ plugins/mining.js
@@ -5,9 +5,9 @@ global.db.data.users[m.sender].money += hasil * 1 // Number(hasil)
    await conn.sendButton(m.chat, `Selamat Kamu Mendapatkan ${hasil}`, '© 𝐚𝐫𝐢𝐚𝐛𝐨𝐭𝐳', 'MINING', `.mining`, m)
 }
 
-handler.help = ['mining', 'nguli']
+handler.help = ['mining']
 handler.tags = ['xp']
-handler.command = /^(mining|nguli)$/i
+handler.command = /^(mining)$/i
 handler.limit = true
 
 module.exports = handler
