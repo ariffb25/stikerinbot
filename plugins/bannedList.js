61b0475afe1da313874bb6b9ab7e465129d9f415
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-24 19:06:53 +0700

    Update bannedList.js

--- plugins/bannedList.js
+++ plugins/bannedList.js
@@ -2,23 +2,21 @@ let handler = async (m, { conn, isOwner }) => {
     let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned)
     let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
     let caption = `
-🌹Daftar Chat Terbanned🌹
+*🌹Chat Terbanned🌹*
 
 📌 Total : ${chats.length} Chat${chats ? '\n' + chats.map(([jid], i) => `
 🔖 ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
 🔖 ${isOwner ? '@' + jid.split`@`[0] : jid}
 `.trim()).join('\n') : ''}
 
-🥀
 
-🌹Daftar Pengguna Terbanned🌹
+*🌹Pengguna Terbanned🌹*
 
 📌 Total : ${users.length} Pengguna${users ? '\n' + users.map(([jid], i) => `
 🔖 ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
 🔖 ${isOwner ? '@' + jid.split`@`[0] : jid}
 `.trim()).join('\n') : ''}
 
-🥀
 `.trim()
     conn.reply(m.chat, caption, m, { contextInfo: { mentionedJid: conn.parseMention(caption) } })
 }
