8687367fe4ccb754bbf5bb77aa8313ad11f25fd2
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-24 11:55:36 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -376,7 +376,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send2ButtobImg(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), '𝑨𝒓𝒊𝒂𝑩𝒐𝒕𝒛', 'Pemilik Bot', '.owner', 'Donasi', '.donasi', m)
+    await conn.send2ButtonImg(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), '𝑨𝒓𝒊𝒂𝑩𝒐𝒕𝒛', 'Pemilik Bot', '.owner', 'Donasi', '.donasi', m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
