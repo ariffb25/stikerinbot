8288a7ab4656d5bae299a98bb1b19a993df97315
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-24 21:59:01 +0700

    Update covid.js

--- plugins/covid.js
+++ plugins/covid.js
@@ -10,7 +10,7 @@ Confirmed : ${json.confirmed.value}
 Recovered : ${json.recovered.value}
 Deaths : ${json.deaths.value}
 Last Update : ${json.lastUpdate}
-\n\n@Fatur`.trim())
+`.trim())
   else throw json
 }
 handler.help = ['covid'].map(v => v + ' <negara>')
