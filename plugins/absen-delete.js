f74ce2cf8cb42a94514f8ff776cc1f7596e341a3
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-24 18:29:26 +0700

    Update absen-delete.js

--- plugins/absen-delete.js
+++ plugins/absen-delete.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     let id = m.chat
     conn.absen = conn.absen ? conn.absen : {}
     if (!(id in conn.absen)) {
-        await conn.sendButton(m.chat, `Tidak ada absen berlangsung digrup ini!\n\nketik *${usedPrefix}mulaiabsen* untuk memulai absen`.trim(), 'Klik di bawah untuk mulai absen!', 'Mulai', `${usedPrefix}mulaiabsen`, m)
+        await conn.sendButton(m.chat, `Tidak ada absen berlangsung digrup ini!\n\nketik *${usedPrefix}mulaiabsen* untuk memulai absen`.trim(), 'Klik di bawah untuk mulai absen!', 'Mulai Absen', `${usedPrefix}mulaiabsen`, m)
         throw false
     }
     delete conn.absen[id]
