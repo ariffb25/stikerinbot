48247adbae3be8cabef0d9015118483985ca064e
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-24 12:18:44 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -30,7 +30,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 *Ukuran File Audio:* ${filesizeF}
 *Ukuran File Video:* ${yt2.filesizeF}
 *Server y2mate:* ${usedServer}
-`.trim(), '© stikerin', 'Audio', `.yta ${vid.url}`, 'Video', `.yt ${vid.url}`)
+`.trim(), '© 𝐚𝐫𝐢𝐚𝐛𝐨𝐭𝐳', 'Audio', `.yta ${vid.url}`, 'Video', `.yt ${vid.url}`)
 }
 handler.help = ['play'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
