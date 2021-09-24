4e5a58a4f34f3c068cd7f7c20bd702ca1c3dabbf
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-24 19:11:44 +0700

    Update blockList.js

--- plugins/blockList.js
+++ plugins/blockList.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn }) => {
     let blocked = conn.blocklist.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
     conn.reply(m.chat, `
-🌹Daftar Terblokir🌹
+*🌹Daftar Terblokir🌹*
 
 📌 Total : ${blocked.length} Pengguna
 ${blocked.map((v, i) => `🔖 ${i + 1}. @${v.split`@`[0]}`).join`\n`}
 
-🥀`.trim(), m, { contextInfo: { mentionedJid: blocked } })
+`.trim(), m, { contextInfo: { mentionedJid: blocked } })
 }
 handler.help = ['blocklist']
 handler.tags = ['owner']
-handler.command = /^listbloc?k|bloc?klist|daftarbloc?k|blocks$/i
+handler.command = /^list?bloc?k|bloc?klist|daftarbloc?k|blocks$/i
 handler.owner = true
 
 module.exports = handler
