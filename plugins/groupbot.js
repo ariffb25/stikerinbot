c9053c0d2c6961273f8849d4fb23880bd7327fb1
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-25 10:30:31 +0700

    Update groupbot.js

--- plugins/groupbot.js
+++ plugins/groupbot.js
@@ -1,9 +1,9 @@
 let handler = async m => m.reply(`
-*Grup Bot Official LUCU-BOT*
+*Grup Official LUCU-BOT*
 
 https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM
 `.trim())
-handler.help = ['groupbot', 'grupbot']
+handler.help = ['grupbot']
 handler.tags = ['info', 'main', 'grup']
 handler.command = /^g(c|ro?up)bot$/i
 
