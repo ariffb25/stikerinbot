acd8cf5ee7b6afb79d5551b1d35f4843ce94e196
Aria Putra Pratama <87540157+iniariaaa@users.noreply.github.com>
2021-09-25 09:43:34 +0700

    Create getpp.js

---
+++ plugins/getpp.js
@@ -0,0 +1,23 @@
+let handler = async (m, { conn, usedPrefix }) => {
+  let pp = 'https://telegra.ph/file/85ce20a1bce70987c67d7.jpg'
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let ohh
+    if (m.isGroup) ohh = m.mentionedJid[0]
+    else ohh = m.chat
+    if (!ohh) throw 'Pp siapa yg mau di ambil bang?'
+  try {
+    pp = await conn.getProfilePicture(who)
+    encodeURIComponent(pp).buffer()
+  } catch (e) {
+  } finally {
+    let str = `
+Imni pp nya @${who.replace(/@.+/, '')}
+`.trim()
+let mentionedJid = [who]
+    conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { quoted: m, sendEphemeral: true, contextInfo: { mentionedJid, forwardingScore: 135, isForwarded: true }})
+  }
+}
+handler.help = ['getpp @user']
+handler.tags = ['tools']
+handler.command = /^getpp$/i
+module.exports = handler
