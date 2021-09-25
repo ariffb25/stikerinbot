dd7f5d600c8f4cef49e822dfea80f3908d0ded50
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-25 19:35:00 +0700

    Update levelup.js

--- plugins/levelup.js
+++ plugins/levelup.js
@@ -28,7 +28,7 @@ let handler = async (m, { conn, usedPrefix }) => {
         .setDiscriminator(discriminator)
       rank.build()
         .then(async data => {
-          await conn.sendButtonImg(m.chat, await (await fecth(pp)) data, `Level *${user.level} (${user.exp - min}/${xp})*\nKurang *${max - user.exp}* lagi!`.trim(), 'Silahkan klik di bawah!', 'Auto Level Up', `${usedPrefix}on autolevelup`, m, { thumbnail: data, height: 282, width: 934 })
+          await conn.sendButtonImg(m.chat, data, `Level *${user.level} (${user.exp - min}/${xp})*\nKurang *${max - user.exp}* lagi!`.trim(), 'Silahkan klik di bawah!', 'Auto Level Up', `${usedPrefix}on autolevelup`, m, { thumbnail: data, height: 282, width: 934 })
         })
     }
     let before = user.level * 1
