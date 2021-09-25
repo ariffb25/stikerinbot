ee4f54b69c78afa005548eafbb5d368fcff99ab1
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-25 10:12:02 +0700

    Update github-search.js

--- plugins/github-search.js
+++ plugins/github-search.js
@@ -23,7 +23,7 @@ ${repo.open_issues} Issue${repo.description ? `
 handler.help = ['githubsearch'].map(v => v + ' <pencarian>')
 handler.tags = ['tools']
 
-handler.command = /^g(ithub|h)search$/i
+handler.command = /^g(ithub|h)?s(earch)?$/i
 
 module.exports = handler
 
