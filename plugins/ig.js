b91580d56876ff302d2815661bc13f13a5875e2e
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-25 13:34:20 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -10,14 +10,14 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     let json = JSON.parse(igdl)
     await m.reply(global.wait)
     for (let { downloadUrl, type } of json) {
-      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '🌹Nih udah di Download^_^🌹', m)
+      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '*🌹Nih udah di Download^_^🌹*', m)
     }
   })
 
 }
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(ig|instagram)$/i
+handler.command = /^(ig(dl)?|instagram)$/i
 
 handler.limit = true
 
