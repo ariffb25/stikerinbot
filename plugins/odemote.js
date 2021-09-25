6d26339092cd3e65ca3b3cf2bd15984341af9912
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-26 04:43:51 +0700

    Update odemote.js

--- plugins/odemote.js
+++ plugins/odemote.js
@@ -3,15 +3,15 @@ let handler = async (m, { conn, participants }) => {
   let users = m.mentionedJid.filter(user => members.includes(user))
   for (let user of users) await conn.groupDemoteAdmin(m.chat, [user]).catch(console.log)
 }
-handler.help = ['demote','member','↓'].map(v => v + ' @user')
-handler.tags = ['admin']
+handler.help = ['demote'].map(v => v + ' @user')
+handler.tags = ['owner']
 
 handler.command = /^(demote|member|↓)$/i
 
 handler.owner = true
 handler.group = true
 
-handler.admin = true
+handler.admin = false
 handler.botAdmin = true
 
 module.exports = handler
