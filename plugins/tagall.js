6eb10ca7c289da336efe8fac87f24cd329231f68
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-27 12:46:58 +0000

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
     throw false
   }
   let users = participants.map(u => u.jid)
-  m.reply(`${text ? `${text}\n` : ''}🌹 *Tag All* 🌹\n\n` + users.map(v => '🔖 @' + v.replace(/@.+/, '')).join`\n` + '\n\ntagall by lucubot\n© rasel👻', null, {
+  m.reply(`${text ? `${text}\n` : ''}🌹 *Tag All* 🌹\n\n` + users.map(v => '🔖 @' + v.replace(/@.+/, '')).join`\n` + '\n```tagall by lucubot```\n```© rasel👻```', null, {
     contextInfo: { mentionedJid: users }
   })
 }
