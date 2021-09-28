39d1ae38433a691866d5890591b341535dbed363
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-28 14:33:50 +0700

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,4 +1,4 @@
-let limit = 50
+let limit = 40
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
