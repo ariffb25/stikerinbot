b8e45594ca02a2af97e4871c9b2202c70d5b5c47
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-28 20:42:11 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -358,7 +358,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.sendButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), '', 'List Menu', '.menu', m)
+    await conn.sendButtonLoc(m.chat, await (await fetch(global.fla + teks)).buffer(), text.trim(), '', 'List Menu', '.menu', m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error v_v', m)
     throw e
