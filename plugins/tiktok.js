56844ec2bb1105f20ca3c574ad27a7360fdee399
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-28 14:53:53 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/yqyjPX/`
   if (!args[0].match(/tiktok/gi)) throw `url salah`
 
-  let res = await fetch(API('hardianto', '/api/download/tiktok', { url: args[0] }, 'apikey'))
+  let res = await fetch(API('zeks', '/api/download/tiktok', { url: args[0] }, 'apikey'))
   if (!res.ok) throw eror
   let json = await res.json()
   // if (!json.status) throw json
