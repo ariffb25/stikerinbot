4405313f27878cd266e5e1b3d51739ac6091d07b
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-30 06:56:23 +0700

    Update _antitroli.js

--- plugins/_antitroli.js
+++ plugins/_antitroli.js
@@ -4,9 +4,9 @@ handler.all = async function (m) {
     if (!db.data.settings[this.user.jid].antispam) return // antitroli aktif?
     if (m.message && m.isBaileys && m.quoted && m.quoted.mtype === 'orderMessage' && !(m.quoted.token && m.quoted.orderId)) {
         m.reply('Njirr jan pake fake troli_-\n\n' + require('util').format(m.key), null)
-        await this.modifyChat(m.chat, 'clear', { //kalo gak mau clear chat blok tiga baris dari sini
-            includeStarred: false
-        }).catch(console.log)
+        //await this.modifyChat(m.chat, 'clear', { //kalo gak mau clear chat blok tiga baris dari sini
+            //includeStarred: false
+        //}).catch(console.log)
         this.reply(global.owner[0] + '@s.whatsapp.net', `
 Pelaku pengirim troli @${m.sender.split`@`[0]}
 ID: ${m.isGroup ? m.chat : m.sender}
