f57d5d1f81d7a5addc62dd2ce2032a544fecd540
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-09-30 10:56:33 +0700

    Add files via upload

---
+++ plugins/_simi.js
@@ -0,0 +1,18 @@
+let fetch = require('node-fetch')
+let handler = m => m
+
+handler.before = async (m) => {
+    let chat = global.db.data.chats[m.chat]
+    if (chat.simi && !chat.isBanned && !m.isGroup) {
+        if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
+        if (!m.text) return
+        let res = await fetch(global.API('pencarikode', '/api/simsimii', { text: encodeURIComponent(m.text) }, 'apikey'))
+        if (!res.ok) return m.reply(eror)
+        let json = await res.json()
+        if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin, ajarin di https://simsimi.com/teach')
+        else await m.reply(`*Simi:* ${json.result}`)
+        return !0
+    }
+    return true
+}
+module.exports = handler
---
+++ plugins/simsimi.js
@@ -0,0 +1,15 @@
+let fetch = require('node-fetch')
+let handler = async (m, { text, usedPrefix, command }) => {
+  if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} hai`
+  let res = await fetch(global.API('pencarikode', '/api/simsimii', { text: encodeURIComponent(text) }, 'apikey'))
+  if (!res.ok) throw eror
+  let json = await res.json()
+  if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin jadi gatau t_t custom pesannya di https://simsimi.com/teach')
+  await m.reply(`*Simi:* ${json.result}`)
+}
+handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
+handler.tags = ['fun']
+handler.command = /^((sim)?simi|simih)$/i
+
+module.exports = handler
+
