e4121f93de7da8e18aced643815520cbe28e80ba
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-10-04 15:10:38 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 *Bot ini menggunakan script github:*
 
-https://github.com/aruffb25/stikerinbot
+https://github.com/ariffb25/stikerinbot
 `.trim())
 handler.help = ['sourcecode']
 handler.tags = ['info']
