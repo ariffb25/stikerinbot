25aba5148d4cf76de8117b2d239aebe959d0f5e0
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-10-05 10:08:48 +0700

    Update citacita.js

--- plugins/citacita.js
+++ plugins/citacita.js
@@ -7,6 +7,7 @@ let handler = async (m, { conn }) => {
   let cita = arr[Math.floor(Math.random() * arr.length)]
   if (!cita) throw false
   await conn.sendFile(m.chat, cita, cita, null, m, 1, { mimetype: 'audio/mp4' })
+  quoted: m, 
 }
 handler.customPrefix = /^cita ?cita$/i
 handler.command = new RegExp
