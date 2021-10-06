32ac775b73f7ae308d0eddfa104ba0d24c917c97
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-10-06 10:31:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = '𝑰𝒕\'𝒔  𝑴𝒆  𝑹𝒂𝒔𝒆𝒍\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nfollback dm👻'
+global.packname = '𝑰𝒕\'𝒔  𝑴𝒆  𝑹𝒂𝒔𝒆𝒍 \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nfollback dm👻'
 global.author = '👻😍🥵😈🥵🤩😝🥵😳🤤🤩😎🥰🥵🥰😍😋☺🥵😈\n\n\n\n\n\n\n\n\n\n\n\n\n@rasel.ganz\n\n\n         😍'
 
 global.wait = '*Tunggu sebentar ya sayang*'
