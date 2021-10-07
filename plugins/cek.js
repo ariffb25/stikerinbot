5194da88c3d8976328845b02b5dcaf47415da082
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-10-07 10:11:30 +0700

    Update cek.js

--- plugins/cek.js
+++ plugins/cek.js
@@ -13,8 +13,8 @@ adalah @${who.replace(/@.+/, '')}
     let mentionedJid = saha.concat(m.mentionedJid)
     conn.reply(m.chat, jawab, m, { contextInfo: { mentionedJid } })
 }
-handler.help = ['', 'anjing', 'babi', 'tolol', 'cantik', 'cantik', 'ganteng', 'gay','lesby', 'homo', 'sange']
+handler.help = ['beban', 'anjing', 'babi', 'tolol', 'cantik', 'ganteng', 'gay','lesby', 'homo', 'sange']
 handler.tags = ['kerang']
-handler.command = /^anjing|babi|tolol|begok|goblok|cantik|ganteng|gay|lesby|homo|sange|honry|tulul$/i
+handler.command = /^beban|anjing|babi|tolol|begok|goblok|cantik|ganteng|gay|lesby|homo|sange|honry|tulul$/i
 
 module.exports = handler
