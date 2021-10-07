16d7294d1f4bbccfd2795b178c6d0edf32fbbdd3
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-10-07 22:24:15 +0700

    Update rasel.js

--- plugins/rasel.js
+++ plugins/rasel.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, participants }) => {
 let users = participants.map((u) => u.jid);
-let zero = fs.readFileSync('./audio/iyaapasyg.mp3')
+let zero = fs.readFileSync('./src/mwah.mp3')
 conn.sendMessage(m.chat, zero, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true, contextInfo: { mentionedJid: users }})
 //conn.fakeReply(m.chat, zero, '0@s.whatsapp.net', '*🔥WOAHHHHH 🔥*', 'status@broadcast')
 }
