58bf986ecf2b8cbc1e311d1f5d738ef69a1206a0
Adhitcantikk <92135768+Adhitcantikk@users.noreply.github.com>
2021-10-08 15:33:08 +0700

    Update bad.js

--- plugins/bad.js
+++ plugins/bad.js
@@ -3,7 +3,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, participants }) => {
 let users = participants.map((u) => u.jid);
 let zero = fs.readFileSync('./src/anjing.mp3')
-conn.sendMessage(m.chat, zero, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true, contextInfo: { mentionedJid: users }})
+conn.sendMessage(m.chat, zero, MessageType.audio, {mimetype: 'audio/mp4', ptt:true})
 //conn.fakeReply(m.chat, zero, '0@s.whatsapp.net', '*WOAHHHHH *', 'status@broadcast')
 }
 handler.customPrefix = /^(babi|anjing|celeng|ngentod|kontol|memek|asu)$/i
