a2df4e80187f9299883e0ac637d8f592c1366338
Adhitcantikk <92135768+Adhitcantikk@users.noreply.github.com>
2021-10-08 14:28:43 +0700

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
     throw false
   }
   let users = participants.map(u => u.jid)
-  m.reply(`${text ? `${text}\n` : ''}🌹 *Tag All* 🌹\n\n` + users.map(v => '🔖 @' + v.replace(/@.+/, '')).join`\n` + '\n\n```tagall by lucu-bot```\n```© rasel👻```', null, {
+  m.reply(`${text ? `${text}\n` : ''}🌹 *Tag All* 🌹\n\n` + users.map(v => '🔖 @' + v.replace(/@.+/, '')).join`\n` + '\n\n```tagall by miku-bot```\n```© adhit```', null, {
     contextInfo: { mentionedJid: users }
   })
 }
