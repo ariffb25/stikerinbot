3a893cc0a3c6ab926f3b84198224d96e7607d31a
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-12 21:37:57 +0530

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,15 +1,15 @@
 let limit = 30
 const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `contoh:\n${usedPrefix + command} https://www.youtube.com/watch?v=yxDdj_G9uRY`
+  if (!args || !args[0]) throw `Example:\n${usedPrefix + command} https://www.youtube.com/watch?v=yxDdj_G9uRY`
   let chat = global.db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : global.wait)
+  m.reply(isLimit ? `File Size: ${filesizeF}\nFile size above ${limit} MB, download by yourself: ${dl_link}` : global.wait)
   if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
-*Judul:* ${title}
-*Ukuran File:* ${filesizeF}
+*Title:* ${title}
+*File Size:* ${filesizeF}
 `.trim(), m, null, {
     asDocument: chat.useDocument, mimetype: 'audio/mp4'
   })
