9da6e41675307fba203e0d792a78fe3f58fe573f
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-13 12:54:55 +0530

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -2,8 +2,8 @@ const { igdl } = require('../lib/scrape')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
-  if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `url salah, perintah ini untuk mengunduh post/reel/tv`
+  if (!args[0]) throw `uhm.. where is the url?\n\nexample:\n${usedPrefix + command} https://www.instagram.com/p/CU0hVEwvBtl/`
+  if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `wrong url, use this command to download post/reel/tv`
 
   igdl(args[0]).then(async res => {
     let igdl = JSON.stringify(res)
