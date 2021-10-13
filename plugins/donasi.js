3f20704b0dea2ecff5491cd6ac50898aff5ba8c8
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-13 12:11:13 +0530

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
-┌〔 Donasi • Emoney 〕
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donate')).buffer(), `
+┌〔 Donate •  〕
+├ *Paytm/gPAY
+├ +917428849575
 └────
-`.trim(), '© stikerin', 'Donasi', '.donasi', m)
-handler.help = ['donasi']
+`.trim(), '© MilfBOT', 'Donate', '.donate', m)
+handler.help = ['donate']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
 
