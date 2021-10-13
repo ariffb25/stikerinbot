4a904bbfd5bace48e8de45baf6d60f65a15d420f
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-13 12:36:32 +0530

    Update addprem.js

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -5,10 +5,10 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
     else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
-    if (json.includes(who.split`@`[0])) throw `${conn.getName(who)} sudah premium!`
+    if (json.includes(who.split`@`[0])) throw `${conn.getName(who)} already premium!`
     json.push(`${who.split`@`[0]}`)
     fs.writeFileSync('./src/premium.json', JSON.stringify(json))
-    m.reply(`${conn.getName(who)} sekarang premium!`)
+    m.reply(`${conn.getName(who)} is now premium!`)
 
     delete require.cache[require.resolve('../config')]
     require('../config')
