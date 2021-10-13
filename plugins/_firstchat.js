ae7776d95d6654770dd04ab3256b9a6b97954153
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-13 14:41:25 +0530

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -12,26 +12,26 @@ handler.all = async function (m) {
     await this.sendButton(m.chat, `
 Hai, ${ucapan()}
 
-${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya bantu?'}
-`.trim(), '© stikerin', user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?', m)
+${user.banned ? 'You are banned' : 'How Can I help you?'}
+`.trim(), '© MilfBOT', user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?', m)
     user.pc = new Date * 1
 }
 
 module.exports = handler
 function ucapan() {
-    const time = moment.tz('Asia/Jakarta').format('HH')
-    res = "Selamat dinihari"
+    const time = moment.tz('Asia/Kolkata').format('HH')
+    res = "Good Morning"
     if (time >= 4) {
-        res = "Selamat pagi"
+        res = "Good Morning"
     }
     if (time > 10) {
-        res = "Selamat siang"
+        res = "Good Afternoon"
     }
     if (time >= 15) {
-        res = "Selamat sore"
+        res = "Good Evening"
     }
     if (time >= 18) {
-        res = "Selamat malam"
+        res = "Good Night"
     }
     return res
 }
