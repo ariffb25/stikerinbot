e1830c354ee3ca159d8f693c7c25e2e127152d85
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-13 13:13:55 +0530

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -35,7 +35,7 @@ handler.before = async function (m) {
                                 .setDiscriminator(discriminator);
                         rank.build()
                                 .then(async data => {
-                                        await this.sendButtonImg(m.chat, data, `_*Level Up!*_\n_${before}_ -> _${user.level}_`.trim(), '© stikerin', 'Daily', ',daily')
+                                        await this.sendButtonImg(m.chat, data, `_*Level Up!*_\n_${before}_ -> _${user.level}_`.trim(), '© MilfBOT', 'Daily', ',daily')
                                 })
                 }
         }
