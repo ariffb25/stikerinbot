689927fd7ae22179575025565def2f8e26955090
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-13 12:49:43 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
     throw false
   }
   let users = participants.map(u => u.jid)
-  m.reply(`${text ? `${text}\n` : ''}┌─〔 Tag All 〕\n` + users.map(v => '├ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
+  m.reply(`${text ? `${text}\n` : ''}┌─〔 Tag All 〕\n\n_Everyone Pay Attention_` + users.map(v => '├ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
     contextInfo: { mentionedJid: users }
   })
 }
