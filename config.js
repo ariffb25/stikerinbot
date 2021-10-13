e2f807da5b32d207995838b707ca9d99d1f50bc7
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-13 06:08:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['917428849575', '6288709645535'] // Letakan nomor kamu disini
+global.owner = ['917428849575', '447441410404'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
