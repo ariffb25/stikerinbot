b61b759079f109052478d2c0ee74d496b61dc804
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-14 22:55:59 +0530

    Update buylimit.js

--- plugins/buylimit.js
+++ plugins/buylimit.js
@@ -3,14 +3,14 @@ let handler = async (m, { conn, command, args }) => {
   let count = command.replace(/^buy/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
-  if (isNaN(count)) throw `hanya angka!\n\ncontoh: .buy5`
+  if (isNaN(count)) throw `only numbers!\n\nexample: .buy5`
   if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
     global.db.data.users[m.sender].exp -= xpperlimit * count
     global.db.data.users[m.sender].limit += count
     conn.reply(m.chat, `-${xpperlimit * count} XP\n+ ${count} Limit`, m)
-  } else conn.reply(m.chat, `XP tidak mencukupi untuk membeli ${count} limit`, m)
+  } else conn.reply(m.chat, `XP is not enough to buy ${count} limit`, m)
 }
-handler.help = ['buy<jumlah limit>', 'buy <jumlah limit>', 'buyall']
+handler.help = ['buy<number limit>', 'buy <number limit>', 'buyall']
 handler.tags = ['xp']
 handler.command = /^buy([0-9]+)|buy|buyall$/i
 handler.owner = false
