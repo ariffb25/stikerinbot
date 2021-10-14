1dbc078251afb573aa09027c9e02beb1596c9dab
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-15 02:15:56 +0530

    Create antitoxic.js

---
+++ plugins/antitoxic.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn, text }) => {
+    let name = m.fromMe ? conn.user : conn.contacts[m.sender]
+
+  conn.reply(m.chat, `
+*「 ANTI TOXIC 」*
+Sender : ${name.vnmae || name.notify || name.name || ('+' + name.jid.split`@`[0])}
+Message : ${m.text}
+Get used to it, don't be toxic! :)
+`.trim(), m)
+    let mentionedJid = [m.sender]
+}
+handler.customPrefix = /bitch|fuck|nude|fucker|pussy|dick|porn|chutiya|bc|sale|madarchod|bkl|gaand|randi|loda|lodi|sex|horny|nigga|fuck|dick|bitch|tits|bastard|poor/i
+handler.command = new RegExp
+
+module.exports = handler
