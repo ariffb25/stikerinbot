3ce76a66320229fafe0b495622ae4ea83070196d
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-15 01:32:13 +0530

    Update wm.js

--- plugins/wm.js
+++ plugins/wm.js
@@ -13,25 +13,25 @@ let handler = async (m, { conn, text }) => {
     if (/webp/.test(mime)) {
       let img = await q.download()
       let out = await webp2png(img)
-      if (!img) throw `balas stiker dengan perintah ${usedPrefix + command} <packname>|<author>`
+      if (!img) throw `reply sticker with command ${usedPrefix + command} <packname>|<author>`
       stiker = await sticker(0, out, packname || '', author || '')
     } else if (/image/.test(mime)) {
       let img = await q.download()
       let link = await uploadImage(img)
-      if (!img) throw `balas gambar dengan perintah ${usedPrefix + command} <packname>|<author>`
+      if (!img) throw `reply image with command ${usedPrefix + command} <packname>|<author>`
       stiker = await sticker(0, link, packname || '', author || '')
     } else if (/video/.test(mime)) {
-      if ((q.msg || q).seconds > 11) throw 'Maksimal 10 detik!'
+      if ((q.msg || q).seconds > 11) throw '10 seconds max!'
       let img = await q.download()
       let link = await uploadFile(img)
-      if (!img) throw `balas video dengan perintah ${usedPrefix + command} <packname>|<author>`
+      if (!img) throw `reply video with command ${usedPrefix + command} <packname>|<author>`
       stiker = await sticker(0, link, packname || '', author || '')
     }
   } finally {
     if (stiker) await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
-    else throw 'Balas stikernya!'
+    else throw 'Reply to the sticker!'
   }
 }
 handler.help = ['wm <packname>|<author>']
