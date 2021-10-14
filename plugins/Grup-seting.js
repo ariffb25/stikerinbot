b26ff04377cd89b622ad49d645c01f362d03f58c
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-14 22:58:37 +0530

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -2,25 +2,25 @@ let { GroupSettingChange } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 	let isClose = {
 		'open': false,
-		'buka': false,
+		'open': false,
 		'on': false,
 		'1': false,
 		'close': true,
-		'tutup': true,
+		'close': true,
 		'off': true,
 		'0': true,
 	}[(args[0] || '')]
 	if (isClose === undefined) {
 		await conn.send2Button(m.chat, `
-contoh:
-${usedPrefix + command} tutup
-${usedPrefix + command} buka
-	`.trim(), '© MilfBOT', 'Buka', ',grup 1', 'Tutup', ',grup 0')
+example:
+${usedPrefix + command} close
+${usedPrefix + command} open
+	`.trim(), '© MilfBOT', 'open', ',grup 1', 'close', ',grup 0')
 		throw false
 	}
 	await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)
 }
-handler.help = ['grup <buka/tutup>']
+handler.help = ['grup <open/close>']
 handler.tags = ['group']
 handler.command = /^(gro?up)$/i
 handler.owner = false
