78c5a5c425a324d5655733bade9f6dcda4250dd0
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-15 02:06:23 +0530

    Update accbang.js

--- plugins/accbang.js
+++ plugins/accbang.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw 'Padahal udah jadi admin'
+  if (m.fromMe) throw 'No'
+  if (isAdmin) throw 'Even though im already an Admin'
   await conn.groupMakeAdmin(m.chat, [m.sender])
 }
 handler.command = /^admin.$/i
