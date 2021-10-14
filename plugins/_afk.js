096e61f828e53b8d05a0ccf8fc3052bffe5b12e6
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-14 23:03:19 +0530

    Update _afk.js

--- plugins/_afk.js
+++ plugins/_afk.js
@@ -3,8 +3,8 @@ handler.before = m => {
   let user = global.db.data.users[m.sender]
   if (user.afk > -1) {
     m.reply(`
-Kamu berhenti AFK${user.afkReason ? ' setelah ' + user.afkReason : ''}
-Selama ${clockString(new Date - user.afk)}
+You quit AFK${user.afkReason ? ' after ' + user.afkReason : ''}
+For ${clockString(new Date - user.afk)}
 `.trim())
     user.afk = -1
     user.afkReason = ''
@@ -17,9 +17,9 @@ Selama ${clockString(new Date - user.afk)}
     if (!afkTime || afkTime < 0) continue
     let reason = user.afkReason || ''
     m.reply(`
-Jangan tag dia!
-Dia sedang AFK ${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}
-Selama ${clockString(new Date - afkTime)}
+Don't tag him!
+He's AFK ${reason ? 'due to ' + reason : 'for no reason'}
+For ${clockString(new Date - afkTime)}
 `.trim())
   }
   return true
