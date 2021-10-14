c9f860ab4d061be3b5afa01e02318469513dc151
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-14 19:33:34 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -377,9 +377,9 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), 'Made with ❤️ by JORDI', 'MiifBOT', `${_p}owner`, 'Donasi', `${_p}donasi`, m)
+    await conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), 'Made with ❤️ by JORDI', 'MiifBOT', `${_p}owner`, 'Donate', `${_p}donate`, m)
   } catch (e) {
-    conn.reply(m.chat, 'Maaf, menu sedang error', m)
+    conn.reply(m.chat, 'Sorry, the menu has an error', m)
     throw e
   }
 }
