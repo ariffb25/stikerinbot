7323dc9b123a6677d8412310750daa04a8b23495
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-15 01:26:25 +0530

    Update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -1,7 +1,7 @@
 let { GroupSettingChange } = require('@adiwajshing/baileys')
 let handler = m => m
 
-let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
+let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?|pussy|dick|porn|chutiya|bc|sale|madarchod|bkl|gaand|randi|loda|lodi|sex|horny|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
 
 handler.before = function (m, { isOwner, isBotAdmin }) {
     if (m.isBaileys && m.fromMe) return !0
@@ -11,14 +11,14 @@ handler.before = function (m, { isOwner, isBotAdmin }) {
 
     if (!chat.antiBadword && !chat.isBanned && isBadword) {
         user.warning += 1
-        this.send2Button(m.chat, `*Badword terdeteksi!*
+        this.send2Button(m.chat, `*Badword Detected*
 Warning: ${user.warning} / 5
-Jika warning mencapai 5 kamu akan dibanned
+If the warning reaches 5 you will be banned
 
-ketik *#on antibadword* untuk menyalakan antibadword
-ketik *#astagfirullah* atau *#maaf* untuk mengurangi warning
+type *#on antibadword* to turn on antibadword
+type *#sorry* to reduce the warning
 
-“Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).`, '© stikerin', 'Nyalakan Antibadword', ',1 antibadword', 'Astaghfirullah', ',maaf', m)
+“Never use a big word when a little filthy one will do” (MilfBOT).`, '© MilfBOT', 'Turn on Antibadword', ',1 antibadword', ',sorry', m)
         if (user.warning >= 5) {
             user.banned = true
             if (m.isGroup) {
