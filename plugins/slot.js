3a76efd82089abca50372847bc2902e52fd5958a
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-15 01:07:02 +0530

    Update slot.js

--- plugins/slot.js
+++ plugins/slot.js
@@ -1,22 +1,22 @@
 let handler = async (m, { args, usedPrefix, command }) => {
     let fa = `
-contoh:
+Example:
 ${usedPrefix + command} 100
 
-artinya kamu bertaruh 100 XP.
+means you bet 100 XP.
 
-*JACKPOT:* taruhan kamu digandakan
-*Kurang beruntung:* +1 XP
-*Kalah:* taruhan kamu diambil`.trim()
+*JACKPOT:* your bet is doubled
+*Unlucky:* +1 XP
+*Lose:* your bet is taken`.trim()
     if (!args[0]) throw fa
     if (isNaN(args[0])) throw fa
     let taruhan = parseInt(args[0])
     let users = global.db.data.users[m.sender]
     let time = users.lastslot + 10000
-    if (new Date - users.lastslot < 10000) throw `tunggu selama ${msToTime(time - new Date())}`
-    if (taruhan < 1) throw 'Minimal 1 XP!'
+    if (new Date - users.lastslot < 10000) throw `wait for ${msToTime(time - new Date())}`
+    if (taruhan < 1) throw 'Minimum 1 XP!'
     if (users.exp < taruhan) {
-        throw `XP kamu tidak cukup!`
+        throw `Your XP is not enough!`
     }
 
     let emojis = ["🏆️", "🥇", "💵"];
@@ -46,10 +46,10 @@ artinya kamu bertaruh 100 XP.
         end = `JACKPOT! 🥳 *+${taruhan + taruhan} XP*`
         users.exp += taruhan
     } else if (a == b || a == c || b == c) {
-        end = `Kurang beruntung 👍 *+1 XP*`
+        end = `Less fortunate 👍 *+1 XP*`
         users.exp += 1
     } else {
-        end = `Kamu kalah 😥 *-${taruhan} XP*`
+        end = `You lose😥 *-${taruhan} XP*`
         users.exp -= taruhan
     }
     users.lastslot = new Date * 1
@@ -60,9 +60,9 @@ ${end}
 
 ${x[0]} ${y[0]} ${z[0]}
 ${x[1]} ${y[1]} ${z[1]}
-${x[2]} ${y[2]} ${z[2]}`.trim(), '© stikerin', `Slot ${args[0]}`, `.slot ${args[0]}`, m)
+${x[2]} ${y[2]} ${z[2]}`.trim(), watermark, `Slot ${args[0]}`, `.slot ${args[0]}`, m)
 }
-handler.help = ['slot <angka>']
+handler.help = ['slot <number>']
 handler.tags = ['game']
 handler.command = /^(slots?)$/i
 module.exports = handler
@@ -77,5 +77,5 @@ function msToTime(duration) {
     minutes = (minutes < 10) ? "0" + minutes : minutes
     seconds = (seconds < 10) ? "0" + seconds : seconds
 
-    return minutes + " menit " + seconds + " detik"
+    return minutes + " minute " + seconds + " seconds"
 }
