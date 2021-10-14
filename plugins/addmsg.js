38e3afe31f170a3acc3349b6683e3cd177af9948
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-15 02:10:54 +0530

    Update addmsg.js

--- plugins/addmsg.js
+++ plugins/addmsg.js
@@ -2,17 +2,17 @@ let { WAMessageProto } = require('@adiwajshing/baileys')
 let handler = async (m, { command, usedPrefix, text }) => {
     let M = WAMessageProto.WebMessageInfo
     let which = command.replace(/add/i, '')
-    if (!m.quoted) throw 'balas pesannya!'
-    if (!text) throw `Gunakan *${usedPrefix}list${which}* untuk melihat daftar nya`
+    if (!m.quoted) throw 'reply to the message!'
+    if (!text) throw `use *${usedPrefix}list${which}* to see the list`
     let msgs = global.db.data.msgs
-    if (text in msgs) throw `'${text}' telah terdaftar di daftar pesan`
+    if (text in msgs) throw `'${text}' registered in the message list`
     msgs[text] = M.fromObject(await m.getQuotedObj()).toJSON()
-    m.reply(`Berhasil menambahkan pesan di daftar pesan sebagai '${text}'
+    m.reply(`Successfully added message in message list as '${text}'
     
-Akses dengan ${usedPrefix}get${which} ${text}`)
+Access with ${usedPrefix}get${which} ${text}`)
 }
 handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker', 'gif'].map(v => 'add' + v + ' <teks>')
 handler.tags = ['database']
-handler.command = /^add(vn|msg|video|audio|img|stic?ker|gif)$/
+handler.command = /^add(vn|msg|video|audio|img|sticker|gif)$/
 
 module.exports = handler
