a6fccd0a7d2dfe7d6c6f1425f04a9d1a847f89a1
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-15 23:11:44 +0530

    Update and rename caripesan.js to searchmsg.js

--- plugins/searchmsg.js
+++ plugins/searchmsg.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `masukan pesan yg ingin dicari!\n\ncontoh:\n${usedPrefix + command} bot|5`
+    if (!text) throw `Enter the message you want to search!\n\nExample:\n${usedPrefix + command} bot|5``
     let split = text.split`|`
     let result = await conn.searchMessages(split[0], m.chat, split[1] ? split[1] : 2, 1)
     if (result.messages.length > 0) {
         let total = result.messages.length
-        let sp = total < Number(split[1]) ? `Hanya ditemukan ${total} pesan ngab` : `Ditemukan ${total} pesan`
+        let sp = total < Number(split[1]) ? `Only ${total} messages were found` : `Found ${total} messages`
         m.reply(sp)
 
         result.messages.map(async ({ key }) => {
@@ -15,9 +15,9 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     }
 }
 
-handler.help = ['caripesan <pesan>|<jumlah>']
+handler.help = ['searchmsg <message>|<anoubt>']
 handler.tags = ['tools']
 
-handler.command = /^caripesan/i
+handler.command = /^searchmsg/i
 
 module.exports = handler
