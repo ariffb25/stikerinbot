d1ef8f0932734417f1d82aba419e881dae7cbe4b
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-15 23:39:26 +0530

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn, isOwner, text, isAdmin }) => {
   try {
     if (who.endsWith('g.us')) global.db.data.chats[who].isBanned = true
     else global.db.data.users[who].banned = true
-    m.reply(`${conn.user.name} not active in chat ${conn.getName(who) == undefined ? 'ini' : conn.getName(who)}.`)
+    m.reply(`${conn.user.name} not active in chat ${conn.getName(who) == undefined ? 'now' : conn.getName(who)}.`)
   } catch (e) {
     throw `number does not exist in database!`
   }
