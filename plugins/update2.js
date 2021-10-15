0d1ce3c295be21b2a88e9a97414669d67bde1fd0
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-16 00:30:29 +0530

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -1,24 +1,57 @@
-let { execSync } = require('child_process')
-let handler = async (m, { conn, text, isROwner }) => {
-  if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/iamenpjordi6/stikerinbot.git && git pull' + (isROwner && text ? ' ' + text : ''))
-    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
-    m.reply(stdout.toString())
-  }
+const { default: fetch } = require('node-fetch')
+const { createWriteStream, existsSync } = require('fs')
+const { promisify } = require('util')
+const { join } = require('path')
+
+let confirmation = {}
+let repository = 'SudoAnirudh/_EVA_'
+let branch = 'master'
+
+async function handler(m, { text }) {
+    let res = await fetch(`https://raw.githubusercontent.com/${repository}/${branch}/${text}`)
+    if (!res.ok) throw await res.text()
+    let filename = join(__dirname, '..', text)
+    if (existsSync(filename)) {
+        confirmation[m.sender] = {
+            res,
+            filename,
+            text,
+            timeout: setTimeout(() => (m.reply('timed out'), delete confirmation[m.sender]), 60000)
+        }
+        throw 'The file already exists, are you sure you want to overwrite? (Y/n) (60s Timeout)'
+    }
+    res.body.pipe(createWriteStream(filename))
+    res.body.once('end', () => {
+        m.reply('Successfully updated!')
+        conn.sendFile(m.chat, filename, text, null, m).catch(console.error)
+    })
 }
-handler.help = ['update']
+
+handler.all = async m => {
+    if (!(m.sender in confirmation)) return
+    let { res, filename, text, timeout } = confirmation[m.sender]
+    if (/^y(es|a)?$/i.test(m.text)) {
+        res.body.pipe(createWriteStream(filename))
+        res.body.once('end', () => {
+            m.reply('Done overwrite!')
+            conn.sendFile(m.chat, filename, text, null, m).catch(console.error)
+        })
+        clearTimeout(timeout)
+        delete confirmation[m.sender]
+        return !0
+    } else if (/^no?$/i.test(m.text)) {
+        delete confirmation[m.sender]
+        m.reply('Rejected')
+        clearTimeout(timeout)
+        return !0
+    }
+}
+
+handler.rowner = true
+handler.help = ['update2']
 handler.tags = ['host']
-handler.command = /^update$/i
-handler.owner = true
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+handler.command = ['update2']
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
+handler.rowner = true
 
 module.exports = handler
