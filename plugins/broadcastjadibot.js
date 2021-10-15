6ac3c10b4b8b3802ebe09ed1bfd7df910c634f95
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-15 09:29:57 +0530

    Update broadcastjadibot.js

--- plugins/broadcastjadibot.js
+++ plugins/broadcastjadibot.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, usedPrefix, text }) => {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
-  let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 Stikerin Broadcast 〕\n\n' + teks)
+  let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 MilfBOT Broadcast 〕\n\n' + teks)
   for (let id of users) {
     await delay(1500)
     await conn.copyNForward(id, content, true)
