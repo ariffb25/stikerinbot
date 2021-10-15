dc555f68d023a94da6ed39e88f4e26a9339c89a3
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-15 09:11:52 +0530

    Update covid.js

--- plugins/covid.js
+++ plugins/covid.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `uhm.. negaranya?\n\ncontoh:\n${usedPrefix + command} indonesia`
+  if (!text) throw `uhm.. for which country?\n\nexample:\n${usedPrefix + command} india`
   let res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/' + (text)))
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
@@ -10,10 +10,10 @@ Confirmed : ${json.confirmed.value}
 Recovered : ${json.recovered.value}
 Deaths : ${json.deaths.value}
 Last Update : ${json.lastUpdate}
-\n\n@Fatur`.trim())
+\n\n@Eva`.trim())
   else throw json
 }
-handler.help = ['covid'].map(v => v + ' <negara>')
+handler.help = ['covid'].map(v => v + ' <country>')
 handler.tags = ['internet']
 handler.command = /^(corona|covid|covid19)$/i
 //susu
