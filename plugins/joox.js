226b33e4812714185586c9631eb7ad6e59462eb5
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-15 09:45:08 +0530

    Update joox.js

--- plugins/joox.js
+++ plugins/joox.js
@@ -1,29 +1,29 @@
 const fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `uhm.. judul nya apa?\n\ncontoh:\n${usedPrefix + command} akad`
-    if (isUrl(text)) throw `uhm.. judul kak bukan pake url\n\ncontoh:\n${usedPrefix + command} akad`
+    if (!text) throw `uhm.. what's the title?\n\nExample:\n${usedPrefix + command} Bad Guy`
+    if (isUrl(text)) throw `uhm.. your title doesn't use url\n\nexample:\n${usedPrefix + command} Bad Guy`
 
-    let res = await fetch(global.API('pencarikode', '/download/joox', { search: text }, 'apikey'))
+    let res = await fetch(global.API('codefinder', '/download/joox', { search: text }, 'apikey'))
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
     if (!json.status) throw json
     let { judul, artist, album, img_url, mp3_url, filesize, duration } = json.result
     let pesan = `
-Judul: ${judul}
-Artis: ${artist}
+Title: ${judul}
+Artist: ${artist}
 Album: ${album}
-Ukuran File: ${filesize}
-Durasi: ${duration}
+File Size: ${filesize}
+Duration: ${duration}
 
-© stikerin
+© Eva
     `.trim()
 
     conn.sendFile(m.chat, img_url, 'eror.jpg', pesan, m, 0, { thumbnail: await (await fetch(img_url)).buffer() })
     conn.sendFile(m.chat, mp3_url, 'error.mp3', '', m, 0, { asDocument: global.db.data.chats[m.chat].useDocument, mimetype: 'audio/mp4' })
 
 }
-handler.help = ['joox'].map(v => v + ' <judul>')
+handler.help = ['joox'].map(v => v + ' <title>')
 handler.tags = ['downloader']
 handler.command = /^joox$/i
 
