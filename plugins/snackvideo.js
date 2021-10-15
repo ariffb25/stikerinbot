744505194aa74f1406e444f5f83ac262ce2115f4
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-15 09:48:00 +0530

    Update snackvideo.js

--- plugins/snackvideo.js
+++ plugins/snackvideo.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'uhm.. url nya mana?'
+  if (!args[0]) throw 'uhm.. where is the url?'
   global.API('xteam', '/dl/', {
     url: args[0]
   }, 'APIKEY')
