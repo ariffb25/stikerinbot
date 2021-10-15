3f8a2f61efa3abb08df5005ec5975055ba86b2f9
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-15 23:43:42 +0530

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -2,9 +2,9 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, text, usedPrefix }) => {
     let [_, code] = text.match(linkRegex) || []
-    if (!code) throw 'Link Salah'
+    if (!code) throw 'Wrong Link'
     let res = await conn.acceptInvite(code)
-    m.reply(`Berhasil join grup ${res.gid}`).then(() => {
+    m.reply(`Successfully joined the group ${res.gid}`).then(() => {
         var jumlahHari = 86400000 * 0.5
         var now = new Date() * 1
         if (now < global.db.data.chats[res.gid].expired) global.db.data.chats[res.gid].expired += jumlahHari
