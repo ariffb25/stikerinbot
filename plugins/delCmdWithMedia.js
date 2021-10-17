823a40e2d85d748ff42f6ce8de7deb47a4228d0f
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-17 12:15:46 +0530

    Update delCmdWithMedia.js

--- plugins/delCmdWithMedia.js
+++ plugins/delCmdWithMedia.js
@@ -1,13 +1,13 @@
 module.exports = Object.assign(async function handler(m, { text }) {
     let hash = text
     if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
-    if (!hash) throw `Tidak ada hash`
+    if (!hash) throw `No hashes`
     let sticker = global.db.data.sticker
-    if (sticker[hash] && sticker[hash].locked) throw 'Kamu tidak memiliki izin untuk menghapus perintah stiker ini'
+    if (sticker[hash] && sticker[hash].locked) throw 'You dont have permission to remove this sticker command'
     delete sticker[hash]
-    m.reply(`Berhasil!`)
+    m.reply(`It worked!`)
 }, {
-    help: ['cmd'].map(v => 'del' + v + ' <teks>'),
+    help: ['cmd'].map(v => 'del' + v + ' <text>'),
     tags: ['database'],
     command: ['delcmd']
 })
