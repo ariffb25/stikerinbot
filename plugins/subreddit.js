e2c2accd5bdea51bf8b2e7bf2ae10e43e56cba74
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-17 10:09:44 +0530

    Update subreddit.js

--- plugins/subreddit.js
+++ plugins/subreddit.js
@@ -2,14 +2,14 @@ let fetch = require("node-fetch")
 
 let handler = async (m, { conn, text }) => {
   let res = await fetch(API('https://meme-api.herokuapp.com', '/gimme/' + encodeURI(text || ''), {}))
-  if (!res.ok) throw eror
+  if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
-  if (!json.url) throw 'Media tidak ditemukan!'
-  if (json.nsfw && !db.data.settings.nsfw) throw 'Mode NSFW tidak aktif'
+  if (!json.url) throw 'Media not found!'
+  if (json.nsfw && !db.data.settings.nsfw) throw 'NSFW mode off'
 
   await conn.sendFile(m.chat, json.url, text, json.title, m, false, { thumbnail: await (await fetch(json.url)).buffer() })
 }
-handler.help = ['subreddit <pencarian>']
+handler.help = ['subreddit <search>']
 handler.tags = ['internet']
 handler.command = /^(sr|subreddit)$/i
 
