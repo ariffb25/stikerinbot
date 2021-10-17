4892b48dd537d2469eb9a9bbeaa376910b617241
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-10-17 09:59:56 +0530

    Update debounce.js

--- plugins/debounce.js
+++ plugins/debounce.js
@@ -2,12 +2,12 @@ let { spawn } = require('child_process');
 let handler = async (m, { conn }) => {
   if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
   if (global.conn.user.jid == conn.user.jid) {
-    await m.reply('Sedang Mereset Bot...\nMohon tunggu sekitar 1 menit')
+    await m.reply('Resetting the Bot...\nPlease wait about 1 minute')
     await global.db.write()
     process.send('reset')
   } else throw '_eeeeeiiittsssss..._'
 }
-handler.help = ['debounce' + (process.send ? '' : ' (Tidak Bekerja)')]
+handler.help = ['debounce' + (process.send ? '' : ' (Does not work)')]
 handler.tags = ['host']
 handler.command = /^debounce$/i
 handler.owner = true
