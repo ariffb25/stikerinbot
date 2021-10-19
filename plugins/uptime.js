3ec7828fcd0e529af762ae6b3f063e1eed2059fd
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-20 06:46:49 +0800

    Create uptime.js

---
+++ plugins/uptime.js
@@ -0,0 +1,24 @@
+let handler = async (m, { conn }) => {
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+
+m.reply(`
+┌─〔 Status 〕
+├ Aktif selama ${uptime}
+└────
+    `.trim())
+}
+handler.help = ['uptime']
+handler.tags = ['info']
+handler.command = /^(uptime)$/i
+
+module.exports = handler
+
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
+
+
