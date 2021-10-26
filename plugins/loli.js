e5091e3d1d3a0aa5f7b5be353031e5f79db08c2b
Ftwrr <76021496+Ftwrr@users.noreply.github.com>
2021-10-26 08:18:39 -0700

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -1,22 +1,11 @@
-var {WAMessageProto} = require('@adiwajshing/baileys')
+let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text }) => {
-heum = await require('node-fetch')('https://raw.githubusercontent.com/Xmell91/loli/master/loli.json').then(v => v.json())
-let url = heum[Math.floor(Math.random() * heum.length)]
- let buttons = [
-  {buttonId: '/loli', buttonText: {displayText: 'Get Again'}, type: 1}
-]
-const buttonsMessage = {
-    contentText: `
-Random Loli
-`.trim(),    footerText: '©stikerin',
-    buttons: buttons,
-  imageMessage: await conn.toMSG({ url }, 'imageMessage'),
-    headerType: 'IMAGE'
-}
-const sendMsg = await conn.prepareMessageFromContent(m.chat,{buttonsMessage},{ quoted: m})
-
-conn.relayWAMessage(sendMsg)
+let res = await fetch('https://raw.githubusercontent.com/Xmell91/loli/master/loli.json')
+if (!res.ok) throw await `${res.status} ${res.statusText}`;
+let json = await res.json();
+let url = json.[Math.floor(Math.random() * json.length)]
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random Loli', '© stikerin', 'Get Again', '/loli', m)
 }
 handler.command = /^(loli)$/i
 handler.tags = ['fun']
