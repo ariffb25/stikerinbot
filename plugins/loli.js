5840788814ad6c551116e68b1e5e5ba127b1e059
Ftwrr <76021496+Ftwrr@users.noreply.github.com>
2021-10-26 08:21:16 -0700

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
 let res = await fetch('https://raw.githubusercontent.com/Xmell91/loli/master/loli.json')
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
-let url = json.[Math.floor(Math.random() * json.length)]
+let url = json[Math.floor(Math.random() * json.length)]
 await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random Loli', '© stikerin', 'Get Again', '/loli', m)
 }
 handler.command = /^(loli)$/i
