9aad256eff6b121fd4963271fd1dab2e422678f4
Ftwrr <76021496+Ftwrr@users.noreply.github.com>
2021-10-29 01:22:03 -0700

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -1,44 +1,27 @@
 const axios = require('axios')
 
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw " please provide place or location name"
+  let res = await axios.get(
+    API("https://api.openweathermap.org", "/data/2.5/weather", {
+      q: args[0],
+      units: "metric",
+      appid: "060a6bcfa19809c2cd4d97a212b19273",
+    })
+  );
+  if (res.status !== 200) throw await `${res.status} ${res.statusText}`
+  if (res.data.cod == 200) await m.reply(`🌸 Name: ${res.data.name}
+💮 Country: ${res.data.sys.country}
+🌈 Weather: ${res.data.weather[0].description}
+🎋 Temperature: ${res.data.main.temp}°C
+💠 Minimum temperature: ${res.data.main.temp_min}°C
+📛 Maximum temperature: ${res.data.main.temp_max}°C
+💦 Humidity: ${res.data.main.humidity}%
+🎐 Wind: ${res.data.wind.speed}km/h`.trim());
+    else throw res.data
+};
+handler.help = ["weather", "cuaca"].map((v) => v + " <Country>");
+handler.tags = ["internet"];
+handler.command = /^(weather|wthr|cuaca)$/i;
 
-
-
-let handler = async (m, { conn, args  , usedPrefix, command })=>{
-
-
-if(!args[0]) throw " please provide place or location name"
-  
-    try{
-
-        const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`)
-        const res = await response
-        
-        const name = res.data.name
-        const Country = res.data.sys.country
-        const Weather= res.data.weather[0].description
-        const Temperature = res.data.main.temp + '°C'
-        const Minimum_Temperature= res.data.main.temp_min + '°C'
-        const Maximum_Temperature= res.data.main.temp_max + '°C'
-        const Humidity= res.data.main.humidity + '%'
-        const Wind= res.data.wind.speed + 'km/h'
-        
-
-        conn.reply(m.chat,`
-        🌸 Place: ${name}\n💮 Country: ${Country}\n🌈 Weather: ${Weather}\n🎋 Temperature: ${Temperature}\n💠 Minimum Temperature: ${Minimum_Temperature}\n📛 Maximum Temperature: ${Maximum_Temperature}\n💦 Humidity: ${Humidity}\n🎐 Wind: ${Wind}
-        `.trim(),m)
-    }catch(e){
-throw 'location not found' 
-console.log(e)
-
-    }
-
-
-
-
-}
-
-handler.help = ['weather']
-handler.tags = ['internet']
-handler.command = /^(weather|wthr)$/i
-
-module.exports = handler
+module.exports = handler;
