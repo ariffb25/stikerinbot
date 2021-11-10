af6b2a38828b6edbcde2b2252ebb9cfc59cf89b2
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-11-11 06:16:04 +0800

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -4,7 +4,8 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw eror
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'Istri kartun', m, 0, { thumbnail: Buffer.alloc(0) })
+  // conn.sendFile(m.chat, json.url, '', 'Istri kartun', m, 0, { thumbnail: Buffer.alloc(0) })
+  await conn.sendButtonImg(m.chat, await(await fetch(json.url)).buffer(), 'pacar kok kartun', 'Ilman', 'Next', '.waifu', m)
 }
 handler.help = ['waifu']
 handler.tags = ['internet']
