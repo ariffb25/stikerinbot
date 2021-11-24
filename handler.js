23e9e3f1cc7289285fba45fdb2a3511fee4a45f8
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2021-11-24 22:23:54 +0700

    Add files via upload

--- handler.js
+++ handler.js
@@ -2,14 +2,12 @@ let util = require('util')
 let fetch = require('node-fetch')
 let simple = require('./lib/simple')
 const uploadImage = require('./lib/uploadImage')
-const knights = require('knights-canvas')
 let { MessageType } = require('@adiwajshing/baileys')
 
 const isNumber = x => typeof x === 'number' && !isNaN(x)
 const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(resolve, ms))
 module.exports = {
   async handler(chatUpdate) {
-    if (global.db.data == null) await global.loadDatabase()
     // console.log(chatUpdate)
     if (!chatUpdate.hasNewMessage) return
     if (!chatUpdate.messages && !chatUpdate.count) return
@@ -30,45 +28,282 @@ module.exports = {
         let user = global.db.data.users[m.sender]
         if (typeof user !== 'object') global.db.data.users[m.sender] = {}
         if (user) {
-          if (!isNumber(user.exp)) user.exp = 0
-          if (!isNumber(user.limit)) user.limit = 10
-          if (!isNumber(user.lastclaim)) user.lastclaim = 0
-          if (!('registered' in user)) user.registered = false
-          if (!user.registered) {
-            if (!('name' in user)) user.name = this.getName(m.sender)
-            if (!isNumber(user.age)) user.age = -1
-            if (!isNumber(user.regTime)) user.regTime = -1
-          }
-          if (!isNumber(user.afk)) user.afk = -1
-          if (!('afkReason' in user)) user.afkReason = ''
-          if (!('banned' in user)) user.banned = false
-          if (!isNumber(user.level)) user.level = 0
-          if (!isNumber(user.call)) user.call = 0
-          if (!user.role) user.role = 'Bronze'
-          if (!('autolevelup' in user)) user.autolevelup = false
-          if (!isNumber(user.pc)) user.pc = 0
-          if (!isNumber(user.warning)) user.warning = 0
-          if (!('pasangan' in user)) user.pasangan = ''
-        } else global.db.data.users[m.sender] = {
-          exp: 0,
-          limit: 10,
-          lastclaim: 0,
-          registered: false,
-          name: this.getName(m.sender),
-          age: -1,
-          regTime: -1,
-          afk: -1,
-          afkReason: '',
-          banned: false,
-          level: 0,
-          call: 0,
-          role: 'Bronze',
-          autolevelup: false,
-          pc: 0,
-          warning: 0,
-          pasangan: '',
-        }
+            if (!isNumber(user.healt)) user.healt = 0
+            if (!isNumber(user.stamina)) user.stamina = 0
+            if (!isNumber(user.level)) user.level = 0
+            if (!isNumber(user.exp)) user.exp = 0
+            if (!isNumber(user.pc)) user.pc = 0
+            if (!isNumber(user.limit)) user.limit = 10
+            if (!isNumber(user.lastclaim)) user.lastclaim = 0
+            if (!isNumber(user.money)) user.money = 0
+            
+            if (!isNumber(user.trofi)) user.trofi= 0
+            if (!user.rtrofi) user.rtrofi = 'Perunggu'
+            if (!isNumber(user.rumahsakit)) user.rumahsakit= 0
+            if (!isNumber(user.fortress)) user.fortress = 0
+            if (!isNumber(user.troopcamp)) user.troopcamp = 0
+            if (!isNumber(user.shield)) user.shield = false
+            if (!isNumber(user.pertanian)) user.pertanian = 0
+            if (!isNumber(user.tambang)) user.tambang = 0
+            
+            if (!isNumber(user.diamond)) user.diamond = 0
+            if (!isNumber(user.iron)) user.iron = 0
 
+            if (!isNumber(user.common)) user.common = 0
+            if (!isNumber(user.uncommon)) user.uncommon = 0
+            if (!isNumber(user.mythic)) user.mythic = 0
+            if (!isNumber(user.legendary)) user.legendary = 0
+            if (!isNumber(user.pet)) user.pet = 0
+            if (!isNumber(user.psepick)) user.psepick = 0
+            if (!isNumber(user.psenjata)) user.psenjata = 0
+        
+            if (!isNumber(user.potion)) user.potion = 0
+            if (!isNumber(user.sampah)) user.sampah = 0
+            if (!isNumber(user.armor)) user.armor = 0
+            if (!isNumber(user.pancing)) user.pancing = 0
+            
+            if (!isNumber(user.kucing)) user.kucing = 0
+            if (!isNumber(user.kucinglastclaim)) user.kucinglastclaim = 0
+            if (!isNumber(user.kuda)) user.kuda = 0
+            if (!isNumber(user.kudalastclaim)) user.kudalastclaim = 0
+            if (!isNumber(user.rubah)) user.rubah = 0
+            if (!isNumber(user.rubahlastclaim)) user.rubahlastclaim = 0
+            if (!isNumber(user.anjing)) user.anjing = 0
+            if (!isNumber(user.anjinglastclaim)) user.anjinglastclaim = 0
+            
+             if (!isNumber(user.apel)) user.apel = 0
+             if (!isNumber(user.ayamb)) user.ayamb = 0
+             if (!isNumber(user.ayamg)) user.ayamg = 0
+             if (!isNumber(user.sapir)) user.sapir = 0
+             if (!isNumber(user.ssapi)) user.ssapi = 0
+             if (!isNumber(user.esteh)) user.esteh = 0
+             if (!isNumber(user.leleb)) user.leleb = 0
+             if (!isNumber(user.leleg)) user.leleg = 0
+
+            if (!'banned' in user) user.banned = false
+            if (!'bannedReason' in user) user.bannedReason = ''
+            if (!isNumber(user.warn)) user.warn = 0
+
+            if (!isNumber(user.afk)) user.afk = -1
+            if (!'afkReason' in user) user.afkReason = ''
+            
+            if (!isNumber(user.pedagang)) user.pedagang = false
+            if (!isNumber(user.polisi)) user.polisi = false
+            if (!isNumber(user.dokter)) user.dokter = false
+            if (!isNumber(user.ojek)) user.ojek = false
+            if (!isNumber(user.petani)) user.petani = false
+            if (!isNumber(user.kuli)) user.kuli = false
+            if (!isNumber(user.montir)) user.montir = false
+            if (!isNumber(user.job)) user.job = false
+            //last Job
+            if (!isNumber(user.lastjb)) user.lastjb = 0
+
+            if (!isNumber(user.nila)) user.nila = 0
+            if (!isNumber(user.bawal)) user.bawal = 0
+            if (!isNumber(user.lele)) user.lele = 0
+            if (!isNumber(user.paus)) user.paus = 0
+            if (!isNumber(user.kepiting)) user.kepiting = 0
+            if (!isNumber(user.gurita)) user.gurita = 0
+            if (!isNumber(user.cumi)) user.cumi= 0
+            if (!isNumber(user.buntal)) user.buntal = 0
+            if (!isNumber(user.dory)) user.dory = 0
+            if (!isNumber(user.lumba)) user.lumba = 0
+            if (!isNumber(user.lobster)) user.lobster = 0
+            if (!isNumber(user.hiu)) user.hiu = 0
+           if (!isNumber(user.udang)) user.udang = 0
+            if (!isNumber(user.ikan)) user.ikan = 0
+            if (!isNumber(user.orca)) user.orca = 0
+
+            if (!isNumber(user.banteng)) user.banteng = 0
+            if (!isNumber(user.harimau)) user.harimau = 0
+            if (!isNumber(user.gajah)) user.gajah = 0
+     if (!isNumber(user.kambing)) user.kambing = 0
+     if (!isNumber(user.panda)) user.panda = 0
+     if (!isNumber(user.buaya)) user.buaya = 0
+     if (!isNumber(user.kerbau)) user.kerbau = 0
+     if (!isNumber(user.sapi)) user.sapi = 0
+     if (!isNumber(user.monyet)) user.monyet = 0
+     if (!isNumber(user.babihutan)) user.babihutan = 0
+     if (!isNumber(user.babi)) user.babi = 0
+     if (!isNumber(user.ayam)) user.ayam = 0
+     if (!isNumber(user.rusa)) user.rusa = 0
+     if (!isNumber(user.keledai)) user.keledai = 0
+     if (!isNumber(user.beruang)) user.beruang = 0
+     if (!isNumber(user.unta)) user.unta = 0
+     //
+      if (!isNumber(user.lastberbru)) user.lastberbru = 0
+            if (!isNumber(user.anakkucing)) user.anakkucing = 0
+            if (!isNumber(user.anakkuda)) user.anakkuda = 0
+            if (!isNumber(user.anakrubah)) user.anakrubah = 0
+            if (!isNumber(user.anakanjing)) user.anakanjing = 0
+            if (!isNumber(user.makananpet)) user.makananpet = 0
+
+            if (!isNumber(user.antispam)) user.antispam = 0
+            if (!isNumber(user.antispamlastclaim)) user.antispamlastclaim = 0
+
+            if (!isNumber(user.kayu)) user.kayu = 0
+            if (!isNumber(user.batu)) user.batu = 0
+            if (!isNumber(user.string)) user.string = 0
+            if (!isNumber(user.sword)) user.sword = 0
+            if (!isNumber(user.sworddurability)) user.sworddurability = 0
+            if (!isNumber(user.pickaxe)) user.pickaxe = 0
+            if (!isNumber(user.pickaxedurability)) user.pickaxedurability = 0
+            if (!isNumber(user.fishingrod)) user.fishingrod = 0
+            if (!isNumber(user.fishingroddurability)) user.fishingroddurability = 0
+
+            if (!isNumber(user.lastadventure)) user.lastadventure = 0
+            if (!isNumber(user.lastfishing)) user.lastfishing = 0
+            if (!isNumber(user.lastdungeon)) user.lastdungeon = 0
+            
+            if (!isNumber(user.lastsda)) user.lastsda = 0
+            if (!isNumber(user.lastsda)) user.lastwar = 0
+            
+            if (!isNumber(user.lastduel)) user.lastduel = 0
+            if (!isNumber(user.lastmining)) user.lastmining = 0
+            if (!isNumber(user.lasthunt)) user.lasthunt = 0
+            if (!isNumber(user.lastweekly)) user.lastweekly = 0
+            if (!isNumber(user.lastmonthly)) user.lastmontly = 0
+            if (!isNumber(user.lastbansos)) user.lastbansos = 0
+            if (!isNumber(user.lastrampok)) user.lastrampok = 0
+            if (!('kingdom' in user)) user.kingdom = false
+            if (!('registered' in user)) user.registered = false
+            if (!user.registered) {
+                if (!('name' in user)) user.name = this.getName(m.sender)
+                if (!isNumber(user.age)) user.age = -1
+                if (!isNumber(user.regTime)) user.regTime = -1
+            }
+            if (!('autolevelup' in user)) user.autolevelup = true
+            if (!('ah' in user)) user.ah = []
+            if (!('mission' in user)) user.mission = {}
+            if (!('pasangan' in user)) user.pasangan = ''
+            if (!user.role) user.role = 'Bronze'
+            if (!isNumber(user.warning)) user.warning = 0
+        } else global.db.data.users[m.sender] = {
+            healt: 100,
+            stamina: 100,
+            level: 0,
+            pc: 0,
+            trofi: 0,
+            rtrofi: 'perunggu',
+            rumahsakit: 0,
+            troopcamp: 0,
+            fortress: 0,
+            makanan: 0,
+            shield: false,
+            exp: 0,
+            limit: 10,
+            lastclaim: 0,
+            money: 0,
+            diamond: 0,
+            iron: 0,
+            common: 0,
+            uncommon: 0,
+            mythic: 0,
+            legendary: 0,
+            pet: 0,
+            psepick: 0,
+            psenjata: 0,
+            potion: 0,
+            pancing: 0,
+            sampah: 0,
+            armor: 0,
+            kucing: 0,
+            kucinglastclaim: 0,
+            kuda: 0,
+            kudalastclaim: 0,
+            rubah: 0,
+            rubahlastclaim: 0,
+            anjing: 0,
+            anjinglastclaim: 0,
+            Banneduser: false,
+            BannedReason: '',
+            warn: 0,
+            afk: -1,
+            afkReason: '',
+            polisi: 0,
+          petani: 0,
+          pedagang: 0,
+          ojek: 0,
+          dokter: 0,
+          kuli: 0,
+          montir: 0,
+          //
+          lele: 0,
+          nila: 0,
+          bawal: 0,
+          paus: 0,
+          kepiting: 0,
+           gurita: 0,
+          cumi: 0,
+          buntal: 0,
+          dory: 0,
+          lumba: 0,
+          lobster: 0,
+          hiu: 0,
+          udang: 0,
+          ikan: 0,
+          orca: 0,
+          banteng: 0,
+          harimau: 0,
+          gajah: 0,
+          kambing: 0,
+          panda: 0,
+          buaya: 0,
+          kerbau : 0,
+          sapi: 0,
+          monyet : 0,
+          babihutan: 0,
+          babi: 0,
+          ayam: 0,
+
+          apel: 20,
+          ayamb: 0,
+          ayamg: 0,
+          ssapi: 0,
+          sapir: 0,
+          leleb: 0,
+          leleg: 0,
+          esteh: 0,
+            anakkucing: 0,
+            anakkuda: 0,
+            anakrubah: 0,
+            anakanjing: 0,
+            makananpet: 0,
+            antispam: 0,
+            antispamlastclaim: 0,
+            string: 0,
+            kayu: 0,
+            batu: 0,
+            besi: 0,
+            emas: 0,
+            makanan: 0,
+            sword: 0,
+            sworddurability: 0,
+            pickaxe: 0,
+            pickaxedurability: 0,
+            fishingrod: 0,
+            fishingroddurability: 0,
+            lastadventure: 0,
+            lastfishing: 0,
+            lastdungeon: 0,
+            lastduel: 0,
+            lastmining: 0,
+            lasthunt: 0,
+            lastweekly: 0,
+            lastmonthly: 0,
+            lastbansos: 0,
+            lastrampok: 0,
+            registered: false,
+            name: this.getName(m.sender),
+            age: -1,
+            regTime: -1,
+            autolevelup: true,
+            ah: [],
+            mission: {},
+            pasangan:'',
+            role: 'Bronze',
+            warning: 0,
+        }
         let chat = global.db.data.chats[m.chat]
         if (typeof chat !== 'object') global.db.data.chats[m.chat] = {}
         if (chat) {
@@ -378,6 +613,7 @@ module.exports = {
   async participantsUpdate({ jid, participants, action }) {
     let chat = global.db.data.chats[jid] || {}
     let text = ''
+    console.log(action);
     switch (action) {
       case 'add':
       case 'remove':
@@ -394,29 +630,41 @@ module.exports = {
             } finally {
               text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Selamat datang, @user!').replace('@subject', this.getName(jid)).replace('@desc', groupMetadata.desc ? String.fromCharCode(8206).repeat(4001) + groupMetadata.desc : '') :
                 (chat.sBye || this.bye || conn.bye || 'Sampai jumpa, @user!')).replace(/@user/g, '@' + user.split`@`[0])
-              let wel = await new knights.Welcome()
-                .setUsername(this.getName(user))
-                .setGuildName(this.getName(jid))
-                .setGuildIcon(ppgc)
-                .setMemberCount(groupMetadata.participants.length)
-                .setAvatar(pp)
-                .setBackground("https://i.ibb.co/KhtRxwZ/dark.png")
-                .toAttachment()
+              let wel, lea;
+              if(!global.UsingCanvasAPI){
+                const knights = require('knights-canvas')
+                wel = await new knights.Welcome()
+                  .setUsername(this.getName(user))
+                  .setGuildName(this.getName(jid))
+                  .setGuildIcon(ppgc)
+                  .setMemberCount(groupMetadata.participants.length)
+                  .setAvatar(pp)
+                  .setBackground("https://i.ibb.co/KhtRxwZ/dark.png")
+                  .toAttachment()
 
-              let lea = await new knights.Goodbye()
-                .setUsername(this.getName(user))
-                .setGuildName(this.getName(jid))
-                .setGuildIcon(ppgc)
-                .setMemberCount(groupMetadata.participants.length)
-                .setAvatar(pp)
-                .setBackground("https://i.ibb.co/KhtRxwZ/dark.png")
-                .toAttachment()
+                lea = await new knights.Goodbye()
+                  .setUsername(this.getName(user))
+                  .setGuildName(this.getName(jid))
+                  .setGuildIcon(ppgc)
+                  .setMemberCount(groupMetadata.participants.length)
+                  .setAvatar(pp)
+                  .setBackground("https://i.ibb.co/KhtRxwZ/dark.png")
+                  .toAttachment();
 
-              this.sendFile(jid, action === 'add' ? wel.toBuffer() : lea.toBuffer(), 'pp.jpg', text, null, false, {
-                contextInfo: {
-                  mentionedJid: [user]
-                }
-              })
+                this.sendFile(jid, action === 'add' ? wel.toBuffer() : lea.toBuffer(), 'pp.jpg', text, null, false, {
+                  contextInfo: {
+                    mentionedJid: [user]
+                  }
+                })
+              } else {
+                wel = `${global.CanvasAPI != '' ? global.canvasAPI : 'https://canvas-heroku-stikerinbot.herokuapp.com'}/generatewelcome?username=${this.getName(user)}&groupname=${this.getName(jid)}&grouplength=${groupMetadata.participants.length}&avatarurl=${pp}&groupavatar=${ppgc}` // If You Want Custom Background Add &bg=URL
+                lea = `${global.CanvasAPI != ''? global.canvasAPI : 'https://canvas-heroku-stikerinbot.herokuapp.com'}/generatebye?username=${this.getName(user)}&groupname=${this.getName(jid)}&grouplength=${groupMetadata.participants.length}&avatarurl=${pp}&groupavatar=${ppgc}` // If You Want Custom Background Add &bg=URL
+                this.sendFile(jid, action === 'add' ? wel : lea, 'pp.jpg', text, null, false, {
+                  contextInfo: {
+                    mentionedJid: [user]
+                  }
+                })
+              }
             }
           }
         }
