87a7e2cb06e9cd361274150d26bb6b276f37b2b5
Melcanz <91192362+Xmell91@users.noreply.github.com>
2021-11-28 17:47:58 +0700

    Fix eror api anto

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -4,12 +4,12 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/yqyjPX/`
   if (!args[0].match(/tiktok/gi)) throw `url salah`
 
-  let res = await fetch(API('hardianto', '/api/download/tiktok', { url: args[0] }, 'apikey'))
+  let res = await fetch(API('amel', '/tiktok', { url: 'https://vt.tiktok.com/yqyjPX/' }, 'apikey'))
   if (!res.ok) throw eror
   let json = await res.json()
-  // if (!json.status) throw json
+  if (json.status != 200) throw json
   await m.reply(wait)
-  await conn.sendFile(m.chat, json.nowm, '', `${json.caption}\n\n© stikerin`, m)
+  await conn.sendFile(m.chat, json.result.link, '', `${json.result.caption}\n\n© stikerin`, m)
 
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
