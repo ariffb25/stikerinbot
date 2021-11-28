0e209d96cc5c172f44ebd10f1fc80460090426e6
Melcanz <91192362+Xmell91@users.noreply.github.com>
2021-11-28 20:15:38 +0700

    Update asupan.js

--- plugins/asupan.js
+++ plugins/asupan.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-conn.sendFile(m.chat, API('amel', '/asupan', {}, 'apikey'), 'asupan.mp4', '@melcanz.io')
+conn.sendFile(m.chat, API('amel', '/asupan', {}, 'apikey'), 'asupan.mp4', '© melcanz x ariffb')
 }
 handler.help = ['asupan']
 handler.tags = ['fun']
