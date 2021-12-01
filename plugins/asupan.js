6a9be6e2250aa09237593a8078951448291cd493
Amel <91192362+melcanz@users.noreply.github.com>
2021-12-02 00:03:23 +0700

    Typo nama api

--- plugins/asupan.js
+++ plugins/asupan.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, API('amel', '/asupan', {}, 'apikey'), 'asupan.mp4', '© melcanz x ariffb')
+  conn.sendFile(m.chat, API('mel', '/asupan', {}, 'apikey'), 'asupan.mp4', '© melcanz x ariffb')
 }
 handler.help = ['asupan']
 handler.tags = ['fun']
