69e8ce871d622d4003c2da72c32122f4a96c7bbf
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2021-12-10 14:56:59 +0700

    Tambah sedikit doang

--- plugins/tutorialinstall.js
+++ plugins/tutorialinstall.js
@@ -2,18 +2,42 @@ let handler  = async (m, { conn, usedPrefix }) => {
   conn.reply(m.chat, `
 ╭「 Tutorial Install Bot 」
 │
-├❒ *TERMUX*
+├❒ *USER TERMUX*
 │
+├ pkg update && pkg upgrade
 ├ pkg install git
 ├ pkg install mc
 ├ pkg install ffmpeg
 ├ pkg install imagemagick
 ├ pkg install node.js
-├ Pkg update && pkg upgrade
 ├ git clone (sc bot)
-├ cd wabot 
-├ npm i @adiwajshing/baileys
-├ npm start
+├ cd (nama sc bot)
+├ npm install -g npm@6.14.14
+├ npm i
+├ node main.js (sesuaikan dengan scrip nya)
+│
+│
+├❒ UNTUK PENGGUNA WINDOWS/VPS/RDP
+│
+├ Unduh & isntal Git https://git-scm.com/downloads
+├ Unduh & Instal NodeJS https://nodejs.org/en/download
+├ Unduh & Instal FFmpeg https://ffmpeg.org/download.html Jangan Lupa Tambahkan FFmpeg ke variabel lingkungan PATH
+├ Unduh & Instal ImageMagick https://imagemagick.org/script/download.php
+│
+├ git clone (sc bot)
+├ cd (nama sc bot)
+├ npm i
+├ npm update
+├ node .
+│
+│
+├❒ UNTUK PENGGUNA HEROKU
+│
+├ Instal Buildpack
+├ heroku/nodejs
+├ https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest.git
+├ https://github.com/DuckyTeam/heroku-buildpack-imagemagick.git
+│
 └──────────
 `.trim(), m)
 }
