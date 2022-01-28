348d9b540d437570c8a236848af9d52339954a3a
Lord-Patrick <82928780+Lord-Patrick@users.noreply.github.com>
2022-01-28 14:29:00 +0700

    Update lirik.js

--- plugins/lirik.js
+++ plugins/lirik.js
@@ -1,11 +1,10 @@
-let fetch = require('node-fetch')
+let finder = require('lyrics-finder')
 let handler = async (m, { text, usedPrefix, command }) => {
   if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} dandelions`
-  let res = await fetch(global.API('bx', '/api/music/liriklagu', { query: text }, 'apikey'))
-  if (!res.ok) throw await `${res.status} ${res.statusText}`
-  let json = await res.json()
-  if (!json.status) throw json
-  m.reply(json.result)
+  let res = await finder("", text)
+  if (!res) throw eror
+
+  m.reply(res)
 }
 handler.help = ['lirik'].map(v => v + ' <teks>')
 handler.tags = ['internet']
