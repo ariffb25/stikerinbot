59fc423f9fe90534639679d5c7b4182e94b04794
Rama76 <80687852+rasssya76@users.noreply.github.com>
2022-04-05 05:53:23 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['628312734012', '6287755080455'] // Letakan nomor kamu disini
+global.owner = ['6281515589573', '62815155895735'] // Letakan nomor kamu disini
 
 global.APIs = { // API Prefix
   // nama: 'https://website'
